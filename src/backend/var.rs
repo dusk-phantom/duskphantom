2b300d5ea2d5834ca0094ead5bc0ebf549c19b5d
cncsmonster <cncsmonster@163.com>
2024-01-03 08:42:44 +0800

    完善var

--- src/backend/var.rs
+++ src/backend/var.rs
@@ -1,23 +1,38 @@
 use super::*;
+
+#[derive(Clone)]
 pub enum Var {
     Prim(PrimVar),
+    Str(Str),
     Arr(ArrVar),
 }
+
+#[derive(Clone)]
 pub enum PrimVar {
     Int(Int),
     Float(Float),
 }
+#[derive(Clone)]
 pub struct Int {
     pub name: String,
-    pub init: i64,
-    pub is_const: bool,
+    pub init: Option<i64>,
 }
+#[derive(Clone)]
+pub struct Str {
+    pub name: String,
+    pub init: Option<String>,
+}
+impl Str {
+    fn gen_asm(&self) -> String {
+        String::new()
+    }
+}
+#[derive(Clone)]
 pub struct Float {
     pub name: String,
-    pub init: f64,
-    pub is_const: bool,
+    pub init: Option<f64>,
 }
-
+#[derive(Clone)]
 pub struct ArrVar {
     pub name: String,
     pub size: usize,
@@ -33,13 +48,17 @@ impl ArrVar {
 impl PrimVar {
     pub fn gen_asm(&self) -> String {
         // TODO
+
         String::new()
     }
 }
 
 impl Var {
     pub fn gen_asm(&self) -> String {
-        // TODO
-        String::new()
+        match self {
+            Var::Prim(prim) => prim.gen_asm(),
+            Var::Str(str) => str.gen_asm(),
+            Var::Arr(arr) => arr.gen_asm(),
+        }
     }
 }
