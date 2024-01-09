ee1d5fd7406cb7922885e1230533e9e98b7dd84f
LJZ <jz.long@qq.com>
2024-01-09 19:40:02 +0800

    将内存池变为静态变量

--- src/middle/ir/context_arena.rs
+++ src/middle/ir/context_arena.rs
@@ -1,33 +1,7 @@
+use std::sync::OnceLock;
+
 use super::*;
-pub struct ContextArena {
-    functions_arena: Arena<Function>,
-    basic_blocks_arena: Arena<BasicBlock>,
-    instructions_arena: Arena<Box<dyn Instruction>>,
-}
 
-impl ContextArena {
-    /// ContextArena的构造函数
-    pub fn new() -> Self {
-        Self {
-            functions_arena: Arena::new(),
-            basic_blocks_arena: Arena::new(),
-            instructions_arena: Arena::new(),
-        }
-    }
-
-    /// 构造一个新的函数
-    pub fn new_function(&mut self, name: &String) -> FunPtr {
-        self.functions_arena
-            .insert(Function::new(name.clone(), self))
-    }
-
-    /// 构造一个新的基本块
-    pub fn new_basic_block(&mut self, name: String) -> BBPtr {
-        self.basic_blocks_arena.insert(BasicBlock::new(name, self))
-    }
-
-    /// 构造一条新的指令
-    pub fn new_instruction(&mut self) -> InstPtr {
-        todo!()
-    }
-}
+static mut CONTEXT_FUNCTION: OnceLock<Arena<Function>> = OnceLock::new();
+static mut CONTEXT_BASIC_BLOCK: OnceLock<Arena<BasicBlock>> = OnceLock::new();
+static mut CONTEXT_INSTRUCTION: OnceLock<Arena<Box<dyn Instruction>>> = OnceLock::new();
