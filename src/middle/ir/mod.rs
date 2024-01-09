152193d5d4e927dbc1b6d87cd2cf3c5adc0a793d
LJZ <jz.long@qq.com>
2024-01-09 16:42:27 +0800

    move ObjPtr from middle to middle/ir

--- src/middle/ir/mod.rs
+++ src/middle/ir/mod.rs
@@ -10,6 +10,48 @@ pub use self::instruction::Instruction;
 pub use self::module::Module;
 
 use self::context_arena::ContextArena;
-use crate::middle::tool::ObjPtr;
 use generational_arena::{Arena, Index};
 use std::{collections::HashMap, pin::Pin};
+
+/// 表示函数指针
+type FunPtr = Index;
+/// 表示基本块指针
+type BBPtr = Index;
+/// 表示指令指针
+type InstPtr = Index;
+
+use std::{
+    ops::{Deref, DerefMut},
+    ptr::NonNull,
+};
+
+#[derive(Clone, Copy)]
+pub struct ObjPtr<T>(NonNull<T>);
+
+impl<T> ObjPtr<T> {
+    pub fn as_ref(&self) -> &T {
+        unsafe { self.0.as_ref() }
+    }
+
+    pub fn as_mut(&mut self) -> &mut T {
+        unsafe { self.0.as_mut() }
+    }
+
+    pub fn new(ptr: &T) -> Self {
+        let ptr = NonNull::from(ptr);
+        Self(ptr)
+    }
+}
+
+impl<T> Deref for ObjPtr<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        self.as_ref()
+    }
+}
+
+impl<T> DerefMut for ObjPtr<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.as_mut()
+    }
+}
