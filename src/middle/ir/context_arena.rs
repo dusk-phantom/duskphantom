302ede1cc6847b55441765e5ea3638c69cc48586
LJZ <jz.long@qq.com>
2024-01-11 21:15:38 +0800

    删除middle/ir/context_arena.rs

--- src/middle/ir/context_arena.rs
+++
@@ -1,8 +0,0 @@
-use super::*;
-
-pub static CONTEXT_FUNCTION: OnceLock<Mutex<Arena<Mutex<Function>>>> = OnceLock::new();
-pub static CONTEXT_BASIC_BLOCK: OnceLock<Mutex<HashMap<Index, Arena<Mutex<BasicBlock>>>>> =
-    OnceLock::new();
-pub static CONTEXT_INSTRUCTION: OnceLock<
-    Mutex<HashMap<Index, Arena<Mutex<Box<dyn Instruction>>>>>,
-> = OnceLock::new();
