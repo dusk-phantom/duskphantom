d53de88dea6f29b1c4dcaa58467d89cdbc35a184
5eqn <491100866@qq.com>
2024-03-19 22:39:26 +0800

    feat: no vec<box> implementation

--- src/frontend/parser.rs
+++ src/frontend/parser.rs
@@ -88,3 +88,36 @@ where
         Ok(output)
     })
 }
+
+pub fn pre0<Input, Output, Error, InnerParser>(
+    mut parser: InnerParser,
+) -> impl Parser<Input, Output, Error>
+where
+    Input: Stream + StreamIsPartial + Compare<char>,
+    Error: ParserError<Input>,
+    InnerParser: Parser<Input, Output, Error>,
+    <Input as Stream>::Token: AsChar,
+{
+    trace("pad", move |input: &mut Input| {
+        let _ = space0(input)?;
+        let output = parser.parse_next(input)?;
+        Ok(output)
+    })
+}
+
+pub fn lrec<I, OI, OR, E, PI, PR, F>(init: PI, rest: PR, mut comb: F) -> impl Parser<I, OI, E>
+where
+    I: Stream + StreamIsPartial + Compare<char>,
+    E: ParserError<I>,
+    F: FnMut(Box<OI>, OR) -> OI,
+    PI: Parser<I, OI, E>,
+    PR: Parser<I, Vec<OR>, E>,
+{
+    (init, rest).map(move |(base, vec): (_, Vec<OR>)| {
+        let mut res = base;
+        for ix in vec {
+            res = comb(Box::new(res), ix);
+        }
+        res
+    })
+}
