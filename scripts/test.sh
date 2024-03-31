a716924648018740615cfcf5791918552fd61265
cncsmonster <cncsmonster@163.com>
2024-03-31 23:00:36 +0800

    fix ci script

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,6 +8,6 @@ cp $task_script ./data/$1/task.sh
 # 修改上述脚本,指定docker run的时候执行 ./data/$1/task.sh
 # 根据实践构建临时container名 如 cpci-yyyy-mm-dd-hh-mm-ss
 container_name=cpci-$(date +%Y-%m-%d-%H-%M-%S)
-docker run --name $container_name -v ./data/$1:/app cpci ./task.sh
+docker run -it --name $container_name -v ./data/$1:/app cpci ./task.sh
 # 执行docker rm tmp但是不输出显示,而是把标准输出重定向到/dev/null
 docker rm $container_name > /dev/null
