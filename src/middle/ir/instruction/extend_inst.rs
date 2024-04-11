e4137132f308f84b79597309c78a25d00e9c5309
LJZ <jz.long@qq.com>
2024-04-11 22:10:56 +0800

    fix 80

--- src/middle/ir/instruction/extend_inst.rs
+++ src/middle/ir/instruction/extend_inst.rs
@@ -22,6 +22,26 @@ impl IRBuilder {
         }
         inst
     }
+
+    pub fn get_itofp(&mut self, src: Operand) -> InstPtr {
+        let mut inst = self.new_instruction(Box::new(ItoFp {
+            manager: InstManager::new(ValueType::Float),
+        }));
+        unsafe {
+            inst.get_manager_mut().add_operand(src);
+        }
+        inst
+    }
+
+    pub fn get_fptoi(&mut self, src: Operand) -> InstPtr {
+        let mut inst = self.new_instruction(Box::new(FpToI {
+            manager: InstManager::new(ValueType::Int),
+        }));
+        unsafe {
+            inst.get_manager_mut().add_operand(src);
+        }
+        inst
+    }
 }
 
 /// zero extend  bool to  int
@@ -80,5 +100,50 @@ impl Instruction for SextTo {
     gen_common_code!(SextTo, SextTo);
     fn gen_llvm_ir(&self) -> String {
         format!("{} = sext i1 {} to i32", self, self.get_src())
+    }
+}
+
+pub struct ItoFp {
+    manager: InstManager,
+}
+
+impl ItoFp {
+    /// Get the operand which will be converted
+    pub fn get_src(&self) -> &Operand {
+        &self.get_operand()[0]
+    }
+    /// Set the operand which will be converted
+    pub unsafe fn set_src(&mut self, src: Operand) {
+        self.manager.set_operand(0, src);
+    }
+}
+
+impl Display for ItoFp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "%itofp_{}", self.get_id())
+    }
+}
+
+impl Instruction for ItoFp {
+    gen_common_code!(ItoFp, ItoFp);
+    fn gen_llvm_ir(&self) -> String {
+        format!("{} = sitofp i32 {} to float", self, self.get_src())
+    }
+}
+
+pub struct FpToI {
+    manager: InstManager,
+}
+
+impl Display for FpToI {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "%fptoi_{}", self.get_id())
+    }
+}
+
+impl Instruction for FpToI {
+    gen_common_code!(FpToI, FpToI);
+    fn gen_llvm_ir(&self) -> String {
+        format!("{} = fptosi float {} to i32", self, self.get_src())
     }
 }
