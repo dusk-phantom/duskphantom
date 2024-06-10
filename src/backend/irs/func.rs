ec6072b8f5ba29d7e30a05dcf911f235ea0b72e5
cncsmonster <cncsmonster@163.com>
2024-06-10 18:08:36 +0800

    :sparkles: impl BBIter for backend and fix corresponding gen asm helper func

--- src/backend/irs/func.rs
+++ src/backend/irs/func.rs
@@ -8,59 +8,82 @@ use rayon::prelude::*;
 pub struct Func {
     name: String,
     args: Vec<String>,
-    // basic blocks
-    bbs: Vec<Block>,
     // entry block
-    entry: String,
+    entry: Block,
+    // basic blocks
+    other_bbs: Vec<Block>,
 }
 
 impl Func {
     pub fn name(&self) -> &str {
         self.name.as_str()
     }
-    pub fn entry(&self) -> Option<&Block> {
-        self.bbs.iter().find(|bb| bb.label() == self.entry)
+    pub fn entry(&self) -> &Block {
+        &self.entry
     }
     pub fn gen_asm(&self) -> String {
-        let entry = self.bbs.iter().find(|bb| bb.label() == self.entry).unwrap();
-        let entry = entry.gen_asm();
         let thread_pool = rayon::ThreadPoolBuilder::new()
             .num_threads(CONFIG.num_parallel_for_block_gen_asm)
             .build()
             .unwrap();
-        let mut other_bbs = self
-            .bbs
-            .iter()
-            .filter(|bb| bb.label() != self.entry)
-            .collect::<Vec<&Block>>();
-        other_bbs.sort_by_cached_key(|bb| bb.label());
-        let other_bbs = thread_pool.install(|| {
-            other_bbs
-                .par_iter()
+        let bbs: Vec<&Block> = self.iter_bbs().collect();
+        let bbs_asm = thread_pool.install(|| {
+            bbs.par_iter()
                 .map(|bb| bb.gen_asm())
                 .collect::<Vec<String>>()
                 .join("\n")
         });
-        GenTool::gen_func(self.name.as_str(), entry.as_str(), other_bbs.as_str())
+        GenTool::gen_func(self.name.as_str(), &bbs_asm)
     }
 }
 
 impl Func {
-    pub fn new(name: String, args: Vec<String>) -> Func {
+    pub fn new(name: String, args: Vec<String>, entry: Block) -> Func {
         Func {
             name,
             args,
-            bbs: Vec::new(),
-            entry: String::new(),
+            other_bbs: Vec::new(),
+            entry,
         }
     }
     pub fn push_bb(&mut self, bb: Block) {
-        self.bbs.push(bb);
+        self.other_bbs.push(bb);
     }
     pub fn extend_bbs(&mut self, bbs: Vec<Block>) {
-        self.bbs.extend(bbs);
+        self.other_bbs.extend(bbs);
     }
-    pub fn set_entry(&mut self, entry: String) {
-        self.entry = entry;
+
+    // iter bbs in a special order,in which entry is the first one,
+    // and other bbs in order of label's dictionary order
+    pub fn iter_bbs(&self) -> BBIter {
+        let mut ordered_bbs = Vec::new();
+        let mut ordered_other_bbs: Vec<&Block> = self.other_bbs.iter().collect();
+        ordered_other_bbs.sort_by_key(|b| b.label());
+        ordered_bbs.extend(ordered_other_bbs.iter());
+        BBIter {
+            entry: &self.entry,
+            ordered_other_bbs: ordered_bbs,
+            idx: 0,
+        }
+    }
+}
+
+pub struct BBIter<'a> {
+    entry: &'a Block,
+    ordered_other_bbs: Vec<&'a Block>,
+    idx: usize,
+}
+impl<'a> Iterator for BBIter<'a> {
+    type Item = &'a Block;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.idx == 0 {
+            self.idx += 1;
+            Some(self.entry)
+        } else {
+            let ret = self.ordered_other_bbs.get(self.idx - 1).cloned();
+            self.idx += 1;
+            ret
+        }
     }
 }
