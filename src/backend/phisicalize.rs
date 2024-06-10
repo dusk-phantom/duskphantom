223167770da754a2bd7b3ae10c6089adc595311b
cncsmonster <cncsmonster@163.com>
2024-06-10 17:23:01 +0800

    :bug: fix clippy

--- src/backend/phisicalize.rs
+++ src/backend/phisicalize.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 // turn virtual backend module to phisic backend module
+#[allow(unused)]
 pub fn phisicalize(mdl: &mut Program) {
     // insert prologue: 在函数开头插入: 开栈、保存callee save寄存器
     // insert epilogue: 在函数返回块插入: 关栈、恢复callee save寄存器
