b58e8d7002803e47040dce85f38ed1819f14b90c
cncsmonster <cncsmonster@163.com>
2024-06-13 22:23:16 +0800

    :pencil: doc about phisicalize backend ir to riscv

--- src/backend/phisicalize.rs
+++ src/backend/phisicalize.rs
@@ -10,13 +10,13 @@ pub fn phisicalize(program: &mut Program) {
 
             // alloc reg: 调用寄存器分配算法,获得分配结果,其中包括寄存器溢出需要的空间
 
-            // count caller save: 计算保存caller save需要的栈空间
+            // count caller save: 计算保存caller save需要的栈空间 max(caller_save(call[i]))
 
             // count callee save: 计算保存callee save需要的栈空间
 
-            // count return value: 计算返回值需要的栈空间,返回值的栈空间以栈底作为起点
+            // count return value: 计算返回值需要的栈空间,返回值的栈空间以栈底作为起点 (略,因为只返回int或者float)
 
-            // process_long_address:检测是否有长地址访问,如果有,则使用保留寄存器处理长地址访问问题 
+            // process_long_address: 检测是否有长地址访问,如果有,则使用保留寄存器处理长地址访问问题 
 
             // apply reg alloc: 实施寄存器分配结果,插入spill 指令,使用t0-t3三个寄存器处理寄存器溢出问题
 
