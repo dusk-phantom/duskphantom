0cca97caac29ed5cde600f3f2a68d3855b69a876
wangfiox <wangfiox@gmail.com>
2024-06-25 01:29:37 +0800

    调了一下开发环境

--- src/backend/from/from_self.rs
+++ src/backend/from/from_self.rs
@@ -104,6 +104,45 @@ pub fn gen(program: &middle::Program) -> Result<prog::Program, BackendError> {
     }
     // dbg!(&global_vars);
 
+    for f in &llvm.functions {
+        // dbg!(&f);
+        let args: Vec<String> = f.params.iter().map(|p| p.name.to_string()).collect();
+        let mut reg_gener = RegGenerator::new();
+        let mut regs: HashMap<Name, Reg> = HashMap::new();
+        let ret_ty = &f.return_type;
+        let mut stack_allocator = StackAllocator::new();
+        let mut stack_slots: HashMap<Name, StackSlot> = HashMap::new();
+        let mut bb = f.entry;
+        // let entry = build_bb(
+        //     bb,
+        //     &mut stack_allocator,
+        //     &mut stack_slots,
+        //     &mut reg_gener,
+        //     &mut regs,
+        // )?;
+        // let mut m_f = Func::new(f.name.to_string(), args, entry);
+        // // dbg!(&ret_ty);
+        // for bb in &f.basic_blocks[1..] {
+        //     let m_bb = build_bb(
+        //         bb,
+        //         &mut stack_allocator,
+        //         &mut stack_slots,
+        //         &mut reg_gener,
+        //         &mut regs,
+        //     )?;
+        //     m_f.push_bb(m_bb);
+        // }
+        // // count stack size,
+        // let stack_size = stack_allocator.allocated();
+        // // align to 16
+        // let stack_size = if stack_size % 16 == 0 {
+        //     stack_size
+        // } else {
+        //     stack_size - (stack_size % 16) + 16
+        // };
+        // funcs.push(m_f);
+    }
+
     // TODO
     Ok(prog::Program {
         entry: None,
