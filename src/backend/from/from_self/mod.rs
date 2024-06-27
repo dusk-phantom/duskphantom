56236a3b03b4b0750b8d8b8d32ad820dfcbbe619
wangfiox <wangfiox@gmail.com>
2024-06-28 00:49:08 +0800

    deleted:    src/backend/from/from_self/from_self.rs 	modified:   src/backend/from/from_self/mod.rs

--- src/backend/from/from_self/mod.rs
+++ src/backend/from/from_self/mod.rs
@@ -5,5 +5,20 @@ mod instruction;
 mod operand;
 
 pub use builder::*;
+#[allow(unused)]
 pub use instruction::*;
+#[allow(unused)]
 pub use operand::*;
+
+use super::super::prog;
+use crate::errors::*;
+use crate::middle;
+
+#[allow(unused)]
+pub fn gen(program: &middle::Program) -> Result<prog::Program, BackendError> {
+    // TODO
+    Ok(prog::Program {
+        entry: None,
+        modules: vec![],
+    })
+}
