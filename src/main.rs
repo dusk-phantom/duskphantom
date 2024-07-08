897dc515c48c6aacdc374cc27872b399c45555a1
cncsmonster <cncsmonster@163.com>
2024-07-08 17:23:26 +0800

    :features: enable bin=compiler to use -l <ll_path> to gen llvm ir

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,14 @@ use compiler::{args::get_args, compile, errors::handle_error};
 fn main() {
     let args = get_args();
     println!("{:?}", args);
-    let (sy_path, output_path, opt_flag, asm_flag) = (
+    let (sy_path, output_path, opt_flag, asm_flag, ll_path) = (
         args.sy_path.unwrap(),
         args.output_path,
         args.opt_flag,
         args.asm_flag,
+        args.ll_path,
     );
-    let result = compile(&sy_path, &output_path, opt_flag, asm_flag);
+    let result = compile(&sy_path, &output_path, opt_flag, asm_flag, ll_path);
     if let Err(err) = result.borrow() {
         handle_error(err);
     }
