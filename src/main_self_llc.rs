69fdefdc5a3fcc6d5f613e521fc9b0ae427c1317
cncsmonster <cncsmonster@163.com>
2024-07-13 12:04:54 +0800

    :features: add gen llvm ir option for compiler-sc

--- src/main_self_llc.rs
+++ src/main_self_llc.rs
@@ -1,27 +1,23 @@
 // use self lib
 extern crate compiler;
 
-
-
-
-
 fn main() {
     #[cfg(feature = "clang_enabled")]
     {
-        use std::borrow::Borrow;
         use compiler::{args::get_args, compile_self_llc, errors::handle_error};
+        use std::borrow::Borrow;
         let args = get_args();
         println!("{:?}", args);
-        let (sy_path, output_path, opt_flag, asm_flag) = (
+        let (sy_path, output_path, opt_flag, asm_flag, ll_path) = (
             args.sy_path.unwrap(),
             args.output_path,
             args.opt_flag,
             args.asm_flag,
+            args.ll_path,
         );
-        let result = compile_self_llc(&sy_path, &output_path, opt_flag, asm_flag);
+        let result = compile_self_llc(&sy_path, &output_path, opt_flag, asm_flag, ll_path);
         if let Err(err) = result.borrow() {
             handle_error(err);
         }
     }
-    
 }
