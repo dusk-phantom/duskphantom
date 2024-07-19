edee21c54e284378b3de54331fa11a75ffa8f5e9
cncsmonster <cncsmonster@163.com>
2024-07-19 14:44:19 +0800

    :features: support using '1.sy -o a.out' args to gen executable file

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use anyhow::Context;
 use backend::checker::CheckValidInst;
 use errors::CompilerError;
-
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
 pub mod args;
 pub mod backend;
 
@@ -59,22 +60,21 @@ pub fn compile(
     }
 
     let asm = program.gen_asm();
-    let output = if !asm_flag { asm2bin(asm) } else { asm };
-    std::fs::write(output_path, output).map_err(CompilerError::IOError)
+    output(asm, output_path, asm_flag)
 }
 
 #[cfg(feature = "clang_enabled")]
 /// compile from clang
 pub fn compile_clang(
-    src_file: &str,
-    output_file: &str,
+    sy_path: &str,
+    output_path: &str,
     opt_flag: bool,
     asm_flag: bool,
     ll_path: Option<String>,
 ) -> Result<(), CompilerError> {
     use errors::BackendError;
 
-    let mut program = clang_frontend::Program::parse(src_file);
+    let mut program = clang_frontend::Program::parse(sy_path);
     if opt_flag {
         clang_frontend::optimize(&mut program)?;
     }
@@ -105,19 +105,18 @@ pub fn compile_clang(
         }
     }
     let asm = program.gen_asm();
-    let output = if !asm_flag { asm2bin(asm) } else { asm };
-    std::fs::write(output_file, output).map_err(CompilerError::IOError)
+    output(asm, output_path, asm_flag)
 }
 
 #[cfg(feature = "clang_enabled")]
 pub fn compile_clang_llc(
-    src_file: &str,
-    output_file: &str,
+    sy_path: &str,
+    output_path: &str,
     opt_flag: bool,
     asm_flag: bool,
     ll_path: Option<String>,
 ) -> Result<(), CompilerError> {
-    let mut program = clang_frontend::Program::parse(src_file);
+    let mut program = clang_frontend::Program::parse(sy_path);
     if opt_flag {
         clang_frontend::optimize(&mut program)?;
     }
@@ -130,8 +129,7 @@ pub fn compile_clang_llc(
         clang_backend::optimize(&mut program);
     }
     let asm = program.gen_asm();
-    let output = if !asm_flag { asm2bin(asm) } else { asm };
-    std::fs::write(output_file, output).map_err(CompilerError::IOError)
+    output(asm, output_path, asm_flag)
 }
 
 #[cfg(feature = "clang_enabled")]
@@ -142,8 +140,6 @@ pub fn compile_self_llc(
     asm_flag: bool,
     ll_path: Option<String>,
 ) -> Result<(), CompilerError> {
-    use std::fs;
-
     let content = std::fs::read_to_string(sy_path).map_err(CompilerError::IOError)?;
     let mut program = frontend::parse(&content)?;
     if opt_flag {
@@ -177,12 +173,23 @@ pub fn compile_self_llc(
         clang_backend::optimize(&mut program);
     }
     let asm = program.gen_asm();
-    let output = if !asm_flag { asm2bin(asm) } else { asm };
-    std::fs::write(output_path, output).map_err(CompilerError::IOError)
+    output(asm, output_path, asm_flag)
+}
+
+fn output(asm: String, output_path: &str, asm_flag: bool) -> Result<(), CompilerError> {
+    if !asm_flag {
+        std::fs::write(output_path, asm2bin(asm)?).map_err(CompilerError::IOError)?;
+        let mut permission = fs::metadata(output_path)?.permissions();
+        permission.set_mode(0o755);
+        fs::set_permissions(output_path, permission)?;
+    } else {
+        std::fs::write(output_path, asm).map_err(CompilerError::IOError)?;
+    };
+    Ok(())
 }
 
 #[allow(unused)]
-pub fn asm2bin(asm: String) -> String {
+pub fn asm2bin(asm: String) -> anyhow::Result<Vec<u8>> {
     // 使用lcc将汇编代码编译成二进制文件
     #[cfg(feature = "clang_enabled")]
     {
@@ -192,11 +199,15 @@ pub fn asm2bin(asm: String) -> String {
         let tmp_bin_path = tmp_bin_file.path();
         let tmp_asm_path = tmp_asm_file.path();
         std::fs::write(tmp_asm_path, asm).expect("msg: write asm failed");
-        let mut cmd = std::process::Command::new("llc");
-        cmd.arg("-o")
-            .arg(tmp_bin_path)
-            .arg(tmp_asm_path)
-            .arg("-Wl,-Ttext=0x80000000");
+
+        // let mut cmd = std::process::Command::new("llc");
+        // cmd.arg("-o")
+        //     .arg(tmp_bin_path)
+        //     .arg(tmp_asm_path)
+        //     .arg("-Wl,-Ttext=0x80000000");
+        let mut cmd = std::process::Command::new("riscv64-linux-gnu-gcc-12");
+        cmd.arg(tmp_asm_path).arg("-o").arg(tmp_bin_path);
+
         let output = cmd.output().expect("msg: exec llc failed");
         if !output.status.success() {
             panic!(
@@ -205,11 +216,7 @@ pub fn asm2bin(asm: String) -> String {
             );
         }
         let bin = std::fs::read(tmp_bin_path).expect("msg: read bin failed");
-        let mut bin_str = String::new();
-        for byte in bin {
-            bin_str.push_str(&format!("{:02x}", byte));
-        }
-        bin_str
+        Ok(bin)
     }
     #[cfg(not(feature = "clang_enabled"))]
     {
