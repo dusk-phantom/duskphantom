5082f0c30d5f2cd4d291608943d668ec06de069b
cncsmonster <cncsmonster@163.com>
2024-07-23 00:18:30 +0800

    :features: impl int array and float array with one dimension

--- src/backend/from/from_llvm/vars.rs
+++ src/backend/from/from_llvm/vars.rs
@@ -1,8 +1,8 @@
 use super::*;
 use builder::IRBuilder;
-use llvm_ir::{constant::Float, Constant, Type};
+use llvm_ir::{constant::Float, Constant, ConstantRef, Type};
 
-use var::Var;
+use var::{ArrVar, Var};
 
 impl IRBuilder {
     pub fn build_global_var(
@@ -19,12 +19,15 @@ impl IRBuilder {
                     Constant::Float(f) => Self::build_float_var(name, f)?,
                     // 处理0初始化的数组
                     Constant::AggregateZero(arr) => Self::build_aggregate_zero_var(name, &arr)?,
+                    Constant::Array {
+                        element_type,
+                        elements,
+                    } => Self::build_array(name, &element_type, &elements)?,
                     _ => {
                         dbg!(&global_var);
                         unimplemented!()
                     }
                 };
-                dbg!(&new_var);
                 global_vars.push(new_var);
             }
         }
@@ -93,6 +96,66 @@ impl IRBuilder {
                     unimplemented!();
                 }
             }
+            _ => unimplemented!(),
+        }
+    }
+
+    /// 处理有初始值的数组
+    pub fn build_array(name: &str, e_ty: &Type, elems: &[ConstantRef]) -> Result<Var> {
+        dbg!(elems);
+        if Self::is_ty_int(e_ty) {
+            let mut inits: Vec<(usize, u32)> = Vec::new();
+            for (idx, e) in elems.iter().enumerate() {
+                let i = Self::build_init_ival(e)?;
+                if i == 0 {
+                    continue;
+                }
+                inits.push((idx, i));
+            }
+            let var = ArrVar::<u32> {
+                name: name.to_string(),
+                capacity: elems.len(),
+                init: inits,
+                is_const: false,
+            };
+            Ok(var.into())
+        } else if Self::is_ty_float(e_ty) {
+            let mut inits: Vec<(usize, f32)> = Vec::new();
+            for (idx, e) in elems.iter().enumerate() {
+                let f = Self::build_init_fval(e)?;
+                // 如果对应二进制位模式为0，则不需要初始化
+                if (f as u32) == 0 {
+                    continue;
+                }
+                inits.push((idx, f));
+            }
+            let var = ArrVar::<f32> {
+                name: name.to_string(),
+                capacity: elems.len(),
+                init: inits,
+                is_const: false,
+            };
+            Ok(var.into())
+        } else {
+            // FIXME:  处理嵌套数组
+            dbg!(e_ty);
+            unimplemented!();
+        }
+    }
+    #[inline]
+    pub fn build_init_ival(elem: &Constant) -> Result<u32> {
+        match elem {
+            Constant::Int { bits: _, value } => Ok(*value as u32),
+            _ => unimplemented!(),
+        }
+    }
+    #[inline]
+    pub fn build_init_fval(elem: &Constant) -> Result<f32> {
+        match elem {
+            Constant::Float(f) => match f {
+                llvm_ir::constant::Float::Single(f) => Ok(*f),
+                _ => unimplemented!(),
+            },
             _ => unimplemented!(),
         }
     }
