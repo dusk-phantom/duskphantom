bb444db23528f48c7a6bc6bb5c127f229e9fbe67
LJZ <jz.long@qq.com>
2024-07-22 23:46:46 +0800

    :sparkles: impl loop recognition. tests are passed

--- src/middle/analysis/loop_tools.rs
+++ src/middle/analysis/loop_tools.rs
@@ -2,6 +2,8 @@ use std::usize;
 
 use crate::utils::mem::{ObjPool, ObjPtr};
 
+use self::domin::make_domin;
+
 use super::*;
 
 type LoopPtr = ObjPtr<LoopTree>;
@@ -34,14 +36,9 @@ pub struct LoopForest {
 
 #[allow(dead_code)]
 impl LoopForest {
-    pub fn new() -> Self {
-        LoopForest {
-            pool: ObjPool::new(),
-            forest: Vec::new(),
-        }
-    }
-
-    pub fn make_forest(&mut self, func: FunPtr) -> Option<LoopForest> {
+    // tarjan算法变体
+    pub fn make_forest(func: FunPtr) -> Option<LoopForest> {
+        let domin = make_domin(func);
         let mut stack;
         if let Some(x) = func.entry {
             stack = vec![x];
@@ -63,7 +60,9 @@ impl LoopForest {
 
                 // dfs
                 if let Some(next) = bb.get_succ_bb().first() {
-                    stack.push(*next);
+                    if !id_map.contains_key(next) {
+                        stack.push(*next);
+                    }
                 }
             } else if bb
                 .get_succ_bb()
@@ -77,26 +76,26 @@ impl LoopForest {
                 let cur_map: HashSet<usize> = bb
                     .get_succ_bb()
                     .iter()
-                    .map(|x| id_map.get(x).map_or(HashSet::new(), |x| x.clone()))
+                    .map(|x| id_map.get(x).unwrap().clone())
                     .reduce(|acc, e| &acc | &e)
                     .unwrap_or(HashSet::new())
                     .into_iter()
-                    .filter(|x| *x <= cur_id)
+                    .filter(|x| *x <= cur_id && domin.is_dominate(loop_tree_meta[*x].0, bb))
                     .collect();
 
                 // 获取当前序号集中最大两个数（如有）
                 let mut max_two = [-1, -1];
                 cur_map.iter().for_each(|&x| {
                     let x = x as i32;
-                    if x > max_two[1] {
-                        if x > max_two[0] {
-                            max_two[1] = max_two[0];
-                            max_two[0] = x;
-                        } else {
-                            max_two[1] = x;
-                        }
+                    if x > max_two[0] {
+                        max_two[1] = max_two[0];
+                        max_two[0] = x;
+                    } else if x > max_two[1] {
+                        max_two[1] = x;
                     }
                 });
+                id_map.insert(bb, cur_map).unwrap();
+                id_map.get_mut(&bb).unwrap().insert(cur_id);
 
                 // 最大的数为当前循环的head bb的id
                 // 次之为父循环的head bb的id
@@ -108,7 +107,9 @@ impl LoopForest {
                     }
                     [x, y] => {
                         loop_tree_meta[x as usize].1.insert(bb);
-                        loop_tree_meta[x as usize].2 = Some(loop_tree_meta[y as usize].0);
+                        if x as usize == cur_id {
+                            loop_tree_meta[x as usize].2 = Some(loop_tree_meta[y as usize].0);
+                        }
                     }
                 }
             } else {
@@ -118,7 +119,10 @@ impl LoopForest {
             }
         }
 
-        let mut forest = LoopForest::new();
+        let mut forest = LoopForest {
+            pool: ObjPool::new(),
+            forest: Vec::new(),
+        };
         let mut forest_map = HashMap::new();
 
         for (head, blocks, parent_loop) in loop_tree_meta.into_iter() {
@@ -133,10 +137,179 @@ impl LoopForest {
                     .get_mut(&par.head)
                     .iter_mut()
                     .for_each(|x| x.sub_loops.push(loop_ptr));
-            }
+            } else {
+                forest.forest.push(loop_ptr);
+            };
             forest_map.insert(head, loop_ptr);
         }
 
+        forest.forest.retain(|x| {
+            x.blocks.len() > 1
+                || !x.sub_loops.is_empty()
+                || x.head.get_succ_bb().iter().any(|succ| *succ == x.head)
+        });
+
         Some(forest)
+    }
+}
+
+#[cfg(test)]
+mod test_loop {
+    use crate::middle::ir::IRBuilder;
+    use std::iter::zip;
+
+    use super::*;
+
+    fn gen_graph(pool: &mut IRBuilder, down_stream: Vec<[i32; 2]>) -> (FunPtr, Vec<BBPtr>) {
+        let bb_vec: Vec<BBPtr> = (0..down_stream.len())
+            .map(|_| pool.new_basicblock("no_name".to_string()))
+            .collect();
+        for (mut bb, down) in zip(bb_vec.clone(), down_stream) {
+            match down {
+                [-1, -1] => {}
+                [t, -1] => bb.set_true_bb(bb_vec[t as usize]),
+                [t, f] => {
+                    bb.set_true_bb(bb_vec[t as usize]);
+                    bb.set_false_bb(bb_vec[f as usize]);
+                }
+            }
+        }
+        let mut func = pool.new_function("no_name".to_string(), crate::middle::ir::ValueType::Void);
+        func.entry = Some(bb_vec[0]);
+        func.exit = Some(bb_vec[bb_vec.len() - 1]);
+        (func, bb_vec)
+    }
+
+    fn gen_forest(bb_vec: Vec<[i32; 2]>) -> (IRBuilder, Option<LoopForest>, Vec<BBPtr>) {
+        let mut pool = IRBuilder::new();
+        let (func, bb_vec) = gen_graph(&mut pool, bb_vec);
+        let forest = LoopForest::make_forest(func);
+        (pool, forest, bb_vec)
+    }
+
+    #[test]
+    fn one_loop() {
+        let (_pool, forest, bb_vec) = gen_forest(vec![[1, -1], [2, 3], [1, -1], [-1, -1]]);
+        assert!(forest.is_some());
+
+        let forest = forest.unwrap();
+        assert_eq!(forest.forest.len(), 1);
+
+        let lo = forest.forest.first().unwrap();
+        assert!(!lo.is_in_loop(&bb_vec[0]));
+        assert!(lo.is_in_loop(&bb_vec[1]));
+        assert!(lo.is_in_loop(&bb_vec[2]));
+        assert!(!lo.is_in_loop(&bb_vec[3]));
+    }
+
+    #[test]
+    fn two_loop() {
+        let (_pool, forest, bb_vec) = gen_forest(vec![[1, -1], [0, 2], [3, -1], [2, 4], [-1, -1]]);
+        assert!(forest.is_some());
+
+        let forest = forest.unwrap();
+        assert_eq!(forest.forest.len(), 2);
+
+        let first = forest.forest[0];
+        assert!(first.is_in_loop(&bb_vec[0]));
+        assert!(first.is_in_loop(&bb_vec[1]));
+        assert!(!first.is_in_loop(&bb_vec[2]));
+        assert!(!first.is_in_loop(&bb_vec[4]));
+
+        let second = forest.forest[1];
+        assert!(second.is_in_loop(&bb_vec[2]));
+        assert!(second.is_in_loop(&bb_vec[3]));
+        assert!(!second.is_in_loop(&bb_vec[4]));
+    }
+
+    #[test]
+    fn conponent_loop() {
+        let (_pool, forest, bb_vec) = gen_forest(vec![
+            [1, -1],
+            [0, 2],
+            [0, 3],
+            [4, -1],
+            [5, -1],
+            [4, 6],
+            [4, -1],
+        ]);
+
+        assert!(forest.is_some());
+        let forest = forest.unwrap();
+        assert_eq!(forest.forest.len(), 2);
+
+        let first = forest.forest[0];
+        assert_eq!(first.blocks.len(), 3);
+        assert!(first.is_in_loop(&bb_vec[0]));
+        assert!(first.is_in_loop(&bb_vec[1]));
+        assert!(first.is_in_loop(&bb_vec[2]));
+
+        let second = forest.forest[1];
+        assert_eq!(second.blocks.len(), 3);
+        assert!(second.is_in_loop(&bb_vec[4]));
+        assert!(second.is_in_loop(&bb_vec[5]));
+        assert!(second.is_in_loop(&bb_vec[6]));
+    }
+
+    #[test]
+    fn branch_loop() {
+        let (_pool, forest, bb_vec) = gen_forest(vec![[1, 2], [3, -1], [3, -1], [0, -1]]);
+
+        assert!(forest.is_some());
+        let forest = forest.unwrap();
+        assert_eq!(forest.forest.len(), 1);
+
+        let lo = forest.forest[0];
+        assert_eq!(lo.blocks.len(), 4);
+        assert_eq!(lo.sub_loops.len(), 0);
+        assert!(lo.parent_loop.is_none());
+        assert!(lo.is_in_cur_loop(&bb_vec[0]));
+        assert!(lo.is_in_cur_loop(&bb_vec[1]));
+        assert!(lo.is_in_cur_loop(&bb_vec[2]));
+        assert!(lo.is_in_cur_loop(&bb_vec[3]));
+    }
+
+    #[test]
+    fn nested_loop() {
+        let (_pool, forest, bb_vec) = gen_forest(vec![[1, -1], [2, -1], [0, 1]]);
+        assert!(forest.is_some());
+        let forest = forest.unwrap();
+        assert_eq!(forest.forest.len(), 1);
+        let lo = forest.forest[0];
+        assert_eq!(lo.blocks.len(), 1);
+        assert_eq!(lo.head, bb_vec[0]);
+        assert!(lo.is_in_cur_loop(&bb_vec[0]));
+
+        assert_eq!(lo.sub_loops.len(), 1);
+        let sub = lo.sub_loops[0];
+        assert_eq!(sub.blocks.len(), 2);
+        assert_eq!(sub.head, bb_vec[1]);
+        assert!(sub.is_in_cur_loop(&bb_vec[1]));
+        assert!(sub.is_in_cur_loop(&bb_vec[2]));
+    }
+
+    #[test]
+    fn nested_branch_loop() {
+        let (_pool, forest, bb_vec) =
+            gen_forest(vec![[1, 5], [2, 3], [4, -1], [4, -1], [1, 0], [-1, -1]]);
+        assert!(forest.is_some());
+        let forest = forest.unwrap();
+        assert_eq!(forest.forest.len(), 1);
+        let lo = forest.forest[0];
+        assert_eq!(lo.head, bb_vec[0]);
+        assert!(lo.is_in_cur_loop(&bb_vec[0]));
+        assert!(lo.is_in_loop(&bb_vec[1]));
+        assert!(lo.is_in_loop(&bb_vec[2]));
+        assert!(lo.is_in_loop(&bb_vec[3]));
+        assert!(lo.is_in_loop(&bb_vec[4]));
+        assert_eq!(lo.blocks.len(), 1);
+        assert_eq!(lo.sub_loops.len(), 1);
+
+        let sub = lo.sub_loops[0];
+        assert_eq!(sub.blocks.len(), 4);
+        assert!(sub.is_in_loop(&bb_vec[1]));
+        assert!(sub.is_in_loop(&bb_vec[2]));
+        assert!(sub.is_in_loop(&bb_vec[3]));
+        assert!(sub.is_in_loop(&bb_vec[4]));
     }
 }
