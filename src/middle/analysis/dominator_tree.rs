136ecbc6e12dad4d91ee3e133d273a20bd14e3a9
5eqn <491100866@qq.com>
2024-07-26 10:57:20 +0800

    :white_check_mark: migrate tests

--- src/middle/analysis/dominator_tree.rs
+++ src/middle/analysis/dominator_tree.rs
@@ -1,9 +1,9 @@
 use std::collections::{HashMap, HashSet};
 
-use crate::middle::ir::BBPtr;
+use crate::middle::ir::{BBPtr, FunPtr};
 
 pub struct DominatorTree {
-    entry: BBPtr,
+    func: FunPtr,
     dom_map: HashMap<BBPtr, HashSet<BBPtr>>,
     idom_map: Option<HashMap<BBPtr, BBPtr>>,
     df_map: Option<HashMap<BBPtr, HashSet<BBPtr>>>,
@@ -11,9 +11,9 @@ pub struct DominatorTree {
 
 #[allow(unused)]
 impl DominatorTree {
-    pub fn new(entry: BBPtr) -> Self {
+    pub fn new(fun: FunPtr) -> Self {
         DominatorTree {
-            entry,
+            func: fun,
             dom_map: HashMap::new(),
             idom_map: None,
             df_map: None,
@@ -73,7 +73,8 @@ impl DominatorTree {
         match self.idom_map {
             Some(ref idoms) => idoms,
             None => {
-                self.idom_map = Some(get_idom_map(self.entry));
+                let entry = self.func.entry.unwrap();
+                self.idom_map = Some(get_idom_map(entry));
                 self.idom_map.as_ref().unwrap()
             }
         }
@@ -83,9 +84,9 @@ impl DominatorTree {
         match self.df_map {
             Some(ref df) => df,
             None => {
-                let entry = self.entry;
+                let func = self.func;
                 let idoms = self.get_idom_map();
-                self.df_map = Some(get_df_map(entry, idoms));
+                self.df_map = Some(get_df_map(func, idoms));
                 self.df_map.as_ref().unwrap()
             }
         }
@@ -94,18 +95,19 @@ impl DominatorTree {
 
 /// Get dominance frontiers of each basic block in the function
 #[allow(unused)]
-fn get_df_map(entry: BBPtr, idoms: &HashMap<BBPtr, BBPtr>) -> HashMap<BBPtr, HashSet<BBPtr>> {
+fn get_df_map(fun: FunPtr, idoms: &HashMap<BBPtr, BBPtr>) -> HashMap<BBPtr, HashSet<BBPtr>> {
     let mut df = HashMap::new();
 
     /// Calculate dominance frontiers
-    for (bb, idom) in idoms.iter() {
-        if bb == idom {
-            continue;
-        }
+    for bb in fun.dfs_iter() {
         for pred in bb.get_pred_bb() {
             let mut runner = *pred;
-            while runner != idoms[bb] {
-                df.entry(runner).or_insert(HashSet::new()).insert(*bb);
+
+            // Hop up from each predecessor until runner is a dominator of bb
+            // For non-entry block, the first hit must be it's immediate dominator
+            // For entry block, the first hit must be itself, as doms(entry) = { entry }
+            while runner != idoms.get(&bb).copied().unwrap_or(bb) {
+                df.entry(runner).or_insert(HashSet::new()).insert(bb);
 
                 // Only update runner if it's not dead block
                 if let Some(new_runner) = idoms.get(&runner) {
@@ -125,7 +127,6 @@ fn get_df_map(entry: BBPtr, idoms: &HashMap<BBPtr, BBPtr>) -> HashMap<BBPtr, Has
 #[allow(unused)]
 fn get_idom_map(entry: BBPtr) -> HashMap<BBPtr, BBPtr> {
     let mut idoms = HashMap::new();
-    idoms.insert(entry, entry);
 
     /// Calculate postorder with dfs
     fn dfs_postorder(
@@ -186,7 +187,7 @@ fn get_idom_map(entry: BBPtr) -> HashMap<BBPtr, BBPtr> {
                 }
             }
         }
-        idoms.insert(*current_bb, new_idom.unwrap());
+        idoms.insert(*current_bb, new_idom.unwrap_or(entry));
     }
 
     // Return idoms
@@ -196,73 +197,154 @@ fn get_idom_map(entry: BBPtr) -> HashMap<BBPtr, BBPtr> {
 #[cfg(test)]
 pub mod tests_dominator_tree {
     use super::*;
-    use crate::middle::Program;
+    use crate::middle::ir::IRBuilder;
+    use std::iter::zip;
 
-    #[test]
-    fn test_get_idom_map() {
-        let mut program = Program::new();
+    struct TestContext {
+        bb_vec: Vec<BBPtr>,
+        dom_tree: DominatorTree,
+    }
 
-        // Construct a nested if-else graph
-        let mut entry = program.mem_pool.new_basicblock("entry".to_string());
-        let mut then = program.mem_pool.new_basicblock("then".to_string());
-        let mut then_then = program.mem_pool.new_basicblock("then_then".to_string());
-        let mut then_alt = program.mem_pool.new_basicblock("then_alt".to_string());
-        let mut alt = program.mem_pool.new_basicblock("alt".to_string());
-        let end = program.mem_pool.new_basicblock("end".to_string());
-        entry.set_true_bb(then);
-        entry.set_false_bb(alt);
-        then.set_true_bb(then_then);
-        then.set_false_bb(then_alt);
-        then_then.set_true_bb(end);
-        then_alt.set_true_bb(end);
-        alt.set_true_bb(end);
+    impl TestContext {
+        fn new(pool: &mut IRBuilder, down_stream: Vec<[i32; 2]>) -> Self {
+            let bb_vec: Vec<BBPtr> = (0..down_stream.len())
+                .map(|_| pool.new_basicblock("no_name".to_string()))
+                .collect();
+            for (mut bb, down) in zip(bb_vec.clone(), down_stream) {
+                match down {
+                    [-1, -1] => {}
+                    [t, -1] => bb.set_true_bb(bb_vec[t as usize]),
+                    [t, f] => {
+                        bb.set_true_bb(bb_vec[t as usize]);
+                        bb.set_false_bb(bb_vec[f as usize]);
+                    }
+                }
+            }
+            let mut fun =
+                pool.new_function("no_name".to_string(), crate::middle::ir::ValueType::Void);
+            fun.entry = Some(bb_vec.first().cloned().unwrap());
+            fun.exit = Some(bb_vec.last().cloned().unwrap());
+            let dom_tree = DominatorTree::new(fun);
+            Self { bb_vec, dom_tree }
+        }
 
-        // Calculate idoms
-        let idoms = get_idom_map(entry);
+        /// Check if dom(i) == j.
+        fn check_dominator(&mut self, i: usize, j: Vec<usize>) {
+            let dom = self.dom_tree.get_dominator(self.bb_vec[i]);
+            let expected: HashSet<BBPtr> = j.into_iter().map(|index| self.bb_vec[index]).collect();
+            assert_eq!(dom, expected);
+        }
 
-        // Check if idoms are correct
-        assert_eq!(idoms[&entry].name, entry.name);
-        assert_eq!(idoms[&then].name, entry.name);
-        assert_eq!(idoms[&then_then].name, then.name);
-        assert_eq!(idoms[&then_alt].name, then.name);
-        assert_eq!(idoms[&alt].name, entry.name);
-        assert_eq!(idoms[&end].name, entry.name);
+        /// Check if idom(i) == j.
+        fn check_idom(&mut self, i: usize, j: Option<usize>) {
+            let idom = self.dom_tree.get_idom(self.bb_vec[i]);
+            let expected = j.map(|index| self.bb_vec[index]);
+            assert_eq!(idom, expected);
+        }
+
+        /// Check if df(i) == j.
+        fn check_df(&mut self, i: usize, j: Vec<usize>) {
+            let df = self.dom_tree.get_df(self.bb_vec[i]);
+            let expected: HashSet<BBPtr> = j.into_iter().map(|index| self.bb_vec[index]).collect();
+            assert_eq!(df, expected);
+        }
     }
 
     #[test]
-    fn test_get_df_map() {
-        let mut program = Program::new();
+    fn basic_test() {
+        // 0 ──► 1
+        let mut pool = IRBuilder::new();
+        let mut ctx = TestContext::new(&mut pool, vec![[1, -1], [-1, -1]]);
+        ctx.check_dominator(0, vec![0]);
+        ctx.check_dominator(1, vec![0, 1]);
+        ctx.check_idom(0, None);
+        ctx.check_idom(1, Some(0));
+        ctx.check_df(0, vec![]);
+        ctx.check_df(1, vec![]);
+    }
 
-        // Construct a nested if-else graph
-        let mut entry = program.mem_pool.new_basicblock("entry".to_string());
-        let mut then = program.mem_pool.new_basicblock("then".to_string());
-        let mut then_then = program.mem_pool.new_basicblock("then_then".to_string());
-        let mut then_alt = program.mem_pool.new_basicblock("then_alt".to_string());
-        let mut alt = program.mem_pool.new_basicblock("alt".to_string());
-        let end = program.mem_pool.new_basicblock("end".to_string());
-        entry.set_true_bb(then);
-        entry.set_false_bb(alt);
-        then.set_true_bb(then_then);
-        then.set_false_bb(then_alt);
-        then_then.set_true_bb(end);
-        then_alt.set_true_bb(end);
-        alt.set_true_bb(end);
+    #[test]
+    fn no_back_edge() {
+        //    ┌─► 1 ──► 3 ──► 4
+        //    │         ▲     │
+        // 0 ─┤         │     │
+        //    │         │     ▼
+        //    └─► 2 ────┴───► 5
+        let mut pool = IRBuilder::new();
+        let mut ctx = TestContext::new(
+            &mut pool,
+            vec![[1, 2], [3, -1], [3, 5], [4, -1], [5, -1], [-1, -1]],
+        );
+        ctx.check_dominator(0, vec![0]);
+        ctx.check_dominator(1, vec![0, 1]);
+        ctx.check_dominator(2, vec![0, 2]);
+        ctx.check_dominator(3, vec![0, 3]);
+        ctx.check_dominator(4, vec![0, 3, 4]);
+        ctx.check_dominator(5, vec![0, 5]);
+        ctx.check_idom(0, None);
+        ctx.check_idom(1, Some(0));
+        ctx.check_idom(2, Some(0));
+        ctx.check_idom(3, Some(0));
+        ctx.check_idom(4, Some(3));
+        ctx.check_idom(5, Some(0));
+        ctx.check_df(0, vec![]);
+        ctx.check_df(1, vec![3]);
+        ctx.check_df(2, vec![3, 5]);
+        ctx.check_df(3, vec![5]);
+        ctx.check_df(4, vec![5]);
+        ctx.check_df(5, vec![]);
+    }
 
-        // Calculate df
-        let idoms = get_idom_map(entry);
-        let df = get_df_map(entry, &idoms);
+    #[test]
+    fn back_edge() {
+        //       ┌──────────┐
+        //       │          │
+        //       │  ┌─► 2 ──┤
+        //       ▼  │       │
+        // 0 ──► 1 ─┤       │
+        //          │       │
+        //          └─► 3 ◄─┘
+        let mut pool = IRBuilder::new();
+        let mut ctx = TestContext::new(&mut pool, vec![[1, -1], [2, 3], [1, 3], [-1, -1]]);
+        ctx.check_dominator(0, vec![0]);
+        ctx.check_dominator(1, vec![0, 1]);
+        ctx.check_dominator(2, vec![0, 1, 2]);
+        ctx.check_dominator(3, vec![0, 1, 3]);
+        ctx.check_idom(0, None);
+        ctx.check_idom(1, Some(0));
+        ctx.check_idom(2, Some(1));
+        ctx.check_idom(3, Some(1));
+        ctx.check_df(0, vec![]);
+        ctx.check_df(1, vec![1]);
+        ctx.check_df(2, vec![1, 3]);
+        ctx.check_df(3, vec![]);
+    }
 
-        // Check if df lengths are correct
-        assert_eq!(df.len(), 4);
-        assert_eq!(df[&then].len(), 1);
-        assert_eq!(df[&then_then].len(), 1);
-        assert_eq!(df[&then_alt].len(), 1);
-        assert_eq!(df[&alt].len(), 1);
-
-        // Check if df contents are correct
-        assert!(df[&then].contains(&end));
-        assert!(df[&then_then].contains(&end));
-        assert!(df[&then_alt].contains(&end));
-        assert!(df[&alt].contains(&end));
+    #[test]
+    fn branch_nested_loop() {
+        //          ┌─► 2 ─┐
+        //          │      │
+        // 0 ──► 1 ─┤      ├─► 4 ─┐
+        // ▲     ▲  │      │      │
+        // │     │  └─► 3 ─┘      │
+        // │     │                │
+        // └─────┴────────────────┘
+        let mut pool = IRBuilder::new();
+        let mut ctx = TestContext::new(&mut pool, vec![[1, -1], [2, 3], [4, -1], [4, -1], [0, 1]]);
+        ctx.check_dominator(0, vec![0]);
+        ctx.check_dominator(1, vec![0, 1]);
+        ctx.check_dominator(2, vec![0, 1, 2]);
+        ctx.check_dominator(3, vec![0, 1, 3]);
+        ctx.check_dominator(4, vec![0, 1, 4]);
+        ctx.check_idom(0, None);
+        ctx.check_idom(1, Some(0));
+        ctx.check_idom(2, Some(1));
+        ctx.check_idom(3, Some(1));
+        ctx.check_idom(4, Some(1));
+        ctx.check_df(0, vec![]);
+        ctx.check_df(1, vec![0, 1]);
+        ctx.check_df(2, vec![4]);
+        ctx.check_df(3, vec![4]);
+        ctx.check_df(4, vec![0, 1]);
     }
 }
