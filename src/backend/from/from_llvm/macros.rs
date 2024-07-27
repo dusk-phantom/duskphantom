a5a9b54b85a9e44f9ba0e2b148c64b01fdbe5830
cncsmonster <cncsmonster@163.com>
2024-07-28 03:58:16 +0800

    :art: fix typos in from_self; :features: improve macros for build inst which allow swap lhs and rhs to decrease number of insts gen from

--- src/backend/from/from_llvm/macros.rs
+++ src/backend/from/from_llvm/macros.rs
@@ -4,28 +4,30 @@ macro_rules! llvm2tac_three_op_usual {
         llvm2tac_three_op_usual!(DenySwap; $tac_inst_ty, $inst, $reg_gener,$regs)
     }};
     (AllowSwap; $tac_inst_ty:ident,$inst:ident, $reg_gener:ident,$regs:ident) => {{
-        let mut f=||->Result<Vec<Inst>>{
-            let mut ret:Vec<Inst>=vec![];
-            let op0 = &Self::value_from(&$inst.operand0, $regs)?;
-            let op1 = &Self::value_from(&$inst.operand1, $regs)?;
-            let dest = $inst.dest.clone();
-            let dst = $reg_gener.gen_virtual_reg(true);
-            $regs.insert(dest, dst);
-            if let (Operand::Reg(op0), Operand::Reg(op1)) = (op0, op1) {
-                let tac_inst = $tac_inst_ty::new(dst.into(), op0.into(), op1.into());
-                ret.push(tac_inst.into());
-            } else if let (Operand::Reg(op0), Operand::Imm(op1)) = (op0, op1) {
-                let tac_inst = $tac_inst_ty::new(dst.into(), op0.into(), op1.into());
-                ret.push(tac_inst.into());
-            } else if let (Operand::Imm(op0), Operand::Reg(op1)) = (op0, op1) {
-                let tac_inst = $tac_inst_ty::new(dst.into(), op1.into(), op0.into());
-                ret.extend([tac_inst.into()]);
-            } else {
-                return Err(anyhow!("operand type not supported")).with_context(|| context!());
-            }
-            Ok(ret)
-        };
-        f()
+        use $crate::backend::irs::*;
+        let mut ret:Vec<Inst>=vec![];
+        let op0 = &Self::value_from(&$inst.operand0, $regs)?;
+        let op1 = &Self::value_from(&$inst.operand1, $regs)?;
+        let dest = $inst.dest.clone();
+        let dst = $reg_gener.gen_virtual_reg(true);
+        $regs.insert(dest, dst);
+        if let (Operand::Reg(op0), Operand::Reg(op1)) = (op0, op1) {
+            let tac_inst = $tac_inst_ty::new(dst.into(), op0.into(), op1.into());
+            ret.push(tac_inst.into());
+        } else if let (Operand::Reg(op0), Operand::Imm(_)) = (op0, op1) {
+            let (rhs,pre_insert)=Self::prepare_rhs(&$inst.operand1,$reg_gener,$regs)?;
+            ret.extend(pre_insert);
+            let tac_inst = $tac_inst_ty::new(dst.into(), op0.into(), rhs);
+            ret.push(tac_inst.into());
+        } else if let (Operand::Imm(_), Operand::Reg(op1)) = (op0, op1) {
+            let (rhs,pre_insert)=Self::prepare_rhs(&$inst.operand0,$reg_gener,$regs)?;
+            ret.extend(pre_insert);
+            let tac_inst = $tac_inst_ty::new(dst.into(), op1.into(), rhs);
+            ret.extend([tac_inst.into()]);
+        } else {
+            return Err(anyhow!("operand type not supported")).with_context(|| context!());
+        }
+        Ok(ret)
     }};
     (DenySwap; $tac_inst_ty:ident,$inst:ident, $reg_gener:ident,$regs:ident) => {{
         let mut ret:Vec<Inst>=vec![];
