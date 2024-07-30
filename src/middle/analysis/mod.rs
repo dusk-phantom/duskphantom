de0edfe22025f530ca72801fc08724586183f92b
cncsmonster <cncsmonster@163.com>
2024-07-30 22:27:03 +0800

    :wrench: update toolchain; :rotating_light: allow clippy warning

--- src/middle/analysis/mod.rs
+++ src/middle/analysis/mod.rs
@@ -2,4 +2,5 @@ use crate::middle::ir::BBPtr;
 use crate::middle::ir::FunPtr;
 use std::collections::{HashMap, HashSet};
 pub mod dominator_tree;
+#[allow(clippy::legacy_numeric_constants)]
 pub mod loop_tools;
