363a244a5bb345b3a03ab53a4fa3e4129f6d7244
LJZ <jz.long@qq.com>
2024-07-30 23:26:21 +0800

    :bug: fix #234

--- src/middle/analysis/mod.rs
+++ src/middle/analysis/mod.rs
@@ -2,5 +2,4 @@ use crate::middle::ir::BBPtr;
 use crate::middle::ir::FunPtr;
 use std::collections::{HashMap, HashSet};
 pub mod dominator_tree;
-#[allow(clippy::legacy_numeric_constants)]
 pub mod loop_tools;
