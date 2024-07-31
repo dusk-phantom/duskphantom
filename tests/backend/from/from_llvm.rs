fbe08fb8207e43fe3c9953eba24bc52bc35b1f1e
cncsmonster <cncsmonster@163.com>
2024-07-31 10:02:06 +0800

    :white_check_mark: 去除对于func的cargo insta 测试,因为每次用clang生成的llvm ir 都会不一样,从而导致生成后端ir不一样,无法稳定测试

--- tests/backend/from/from_llvm.rs
+++ tests/backend/from/from_llvm.rs
@@ -4,7 +4,7 @@ mod from_llvm_tests {
 
     use anyhow::Result;
     use compiler::{backend, clang_frontend};
-    use insta::{assert_debug_snapshot, assert_snapshot};
+    use insta::assert_debug_snapshot;
 
     fn parse_to_backend(code: &str) -> Result<backend::Program> {
         let tmp_cfile = tempfile::Builder::new().suffix(".c").tempfile()?;
@@ -13,11 +13,14 @@ mod from_llvm_tests {
         let program: backend::Program = backend::from_llvm::gen_from_clang(&front)?;
         Ok(program)
     }
+
+    #[allow(unused)]
     fn parse_to_single_module(code: &str) -> Result<backend::Module> {
         let prog = parse_to_backend(code)?;
         Ok(prog.modules.into_iter().next().unwrap())
     }
 
+    #[allow(unused)]
     fn find_func<'a>(module: &'a backend::Module, name: &str) -> Option<&'a backend::Func> {
         module.funcs.iter().find(|f| f.name() == name)
     }
@@ -156,77 +159,6 @@ mod from_llvm_tests {
                 },
             ),
         ]
-        "###);
-    }
-
-    #[test]
-    #[cfg(feature = "gen_virtual_asm")]
-    fn test_extra_params_func() {
-        let code = indoc::indoc! {r#"
-            int testParam16(int a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7,
-                int a8, int a9, int a10, int a11) {
-                    return a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10 + a11;
-            }
-        "#};
-        let module = parse_to_single_module(code).unwrap();
-        let func = find_func(&module, "testParam16").unwrap();
-        assert_snapshot!(func.gen_asm(), @r###"
-        .text
-        .align	3
-        .globl	testParam16
-        .type	testParam16, @function
-        testParam16:
-        testParam16_.LBB_12:
-        mv x32,a0
-        mv x33,a1
-        mv x34,a2
-        mv x35,a3
-        mv x36,a4
-        mv x37,a5
-        mv x38,a6
-        mv x39,a7
-        ld x40,0(s0)
-        ld x41,8(s0)
-        ld x42,16(s0)
-        ld x43,24(s0)
-        store x32,[72-80]
-        store x33,[64-72]
-        store x34,[88-96]
-        store x35,[48-56]
-        store x36,[24-32]
-        store x37,[32-40]
-        store x38,[56-64]
-        store x39,[0-8]
-        store x40,[40-48]
-        store x41,[16-24]
-        store x42,[8-16]
-        store x43,[80-88]
-        load x44,[72-80]
-        load x45,[64-72]
-        add x46,x44,x45
-        load x47,[88-96]
-        add x48,x46,x47
-        load x49,[48-56]
-        add x50,x48,x49
-        load x51,[24-32]
-        add x52,x50,x51
-        load x53,[32-40]
-        add x54,x52,x53
-        load x55,[56-64]
-        add x56,x54,x55
-        load x57,[0-8]
-        add x58,x56,x57
-        load x59,[40-48]
-        add x60,x58,x59
-        load x61,[16-24]
-        add x62,x60,x61
-        load x63,[8-16]
-        add x64,x62,x63
-        load x65,[80-88]
-        add x66,x64,x65
-        mv a0,x66
-        ret
-        .size	testParam16, .-testParam16
         "###);
     }
 }
