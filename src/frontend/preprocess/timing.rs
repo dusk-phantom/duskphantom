b68c4e7e2b193988f6972359afb44f10d3518f43
5eqn <491100866@qq.com>
2024-07-31 17:46:13 +0800

    :bug: fix test and start

--- src/frontend/preprocess/timing.rs
+++ src/frontend/preprocess/timing.rs
@@ -1,10 +1,10 @@
 pub fn process(input: &str) -> String {
-    // Replace `(\W)starttime\(\)` with `$1_sysy_starttime($LN)`
-    let regex = regex::Regex::new(r"(\W)starttime\(\)").unwrap();
+    // Replace `(\W?)starttime\(\)` with `$1_sysy_starttime($LN)`
+    let regex = regex::Regex::new(r"(\W?)starttime\(\)").unwrap();
     let replaced = regex.replace_all(input, "${1}_sysy_starttime($$LN)");
 
-    // Replace `(\W)stoptime\(\)` with `$1_sysy_stoptime($LN)`
-    let regex = regex::Regex::new(r"(\W)stoptime\(\)").unwrap();
+    // Replace `(\W?)stoptime\(\)` with `$1_sysy_stoptime($LN)`
+    let regex = regex::Regex::new(r"(\W?)stoptime\(\)").unwrap();
     let replaced = regex.replace_all(&replaced, "${1}_sysy_stoptime($$LN)");
 
     // Replace `$LN` with real line number
@@ -26,22 +26,21 @@ pub mod tests_timing {
     #[test]
     fn test_timing() {
         let code = r#"
-        int main() {
-            starttime();
-            starttime();
-            starttime();x1starttime();starttime();
-            stoptime();stoptime();_stoptime();
-        }
-        "#;
-        assert_snapshot!(process(code), @r###"
-
-                int main() {
-                    _sysy_starttime(3);
-                    _sysy_starttime(4);
-                    _sysy_starttime(5);x1starttime();_sysy_starttime(5);
-                    _sysy_stoptime(6);_sysy_stoptime(6);_stoptime();
-                }
-                
+        starttime();
+        starttime();
+        starttime();x1starttime();starttime();
+        stoptime();stoptime();_stoptime();
+        "#
+        .trim()
+        .split('\n')
+        .map(|s| s.trim())
+        .collect::<Vec<_>>()
+        .join("\n");
+        assert_snapshot!(process(&code), @r###"
+        _sysy_starttime(1);
+        _sysy_starttime(2);
+        _sysy_starttime(3);x1_sysy_starttime(3);_sysy_starttime(3);
+        _sysy_stoptime(4);_sysy_stoptime(4);__sysy_stoptime(4);
         "###);
     }
 }
