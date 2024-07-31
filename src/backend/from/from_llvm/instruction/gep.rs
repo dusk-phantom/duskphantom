211930095d403279f784d0385f31a0d1ecf57bb5
cncsmonster <cncsmonster@163.com>
2024-08-01 00:34:29 +0800

    :construction: from_llvm: impl prepare gep address

--- src/backend/from/from_llvm/instruction/gep.rs
+++ src/backend/from/from_llvm/instruction/gep.rs
@@ -44,46 +44,27 @@ impl IRBuilder {
         };
         let dims = Self::dims_from_gep_inst(gep)?;
         let mut offset = reg_gener.gen_virtual_usual_reg();
-        dbg!(gep.to_string());
-        dbg!(&dims);
 
-        // case 1: valid indices could last with only one local var idx, if there is a var idx, it must be the first one
-        // case 2: valid indices could only contains imm.
-        // FIXME: update dims to sub item
-        let mut first_var_idx_occur = false;
-        if let Some(idx) = gep.indices.first() {
-            let idx = Self::value_from(idx, regs)?;
+        // process first index
+        let Some(first_idx) = gep.indices.first() else {
+            unreachable!();
+        };
+        let (new_offset, insts) =
+            Self::process_first_idx(offset, first_idx, &dims, reg_gener, regs)?;
+        ret.extend(insts);
+        offset = new_offset;
 
-            let factor: Imm = dims.size().try_into()?;
-            let (factor, pre_insert) = Self::prepare_imm_rhs(&factor, reg_gener)?;
-            ret.extend(pre_insert);
-
-            match idx {
-                Operand::Imm(imm) => {
-                    let to_add = reg_gener.gen_virtual_usual_reg();
-                    let idx = reg_gener.gen_virtual_usual_reg();
-                    let new_offset = reg_gener.gen_virtual_usual_reg();
-
-                    let li: Inst = LiInst::new(idx.into(), imm.into()).into();
-                    ret.push(li);
-
-                    let mul: Inst = MulInst::new(to_add.into(), idx.into(), factor).into();
-                    ret.push(mul);
-
-                    let add: Inst =
-                        AddInst::new(new_offset.into(), offset.into(), to_add.into()).into();
-                    ret.push(add);
-
-                    offset = new_offset;
-
-                    // update dims
-                }
-                Operand::Reg(reg) => {}
-                _ => unimplemented!(),
+        let mut dims = Some(&dims);
+        for idx in gep.indices.iter() {
+            if let Some(d) = dims {
+                let (new_offset, new_dims, insts) =
+                    Self::process_sub_idx(offset, idx, d, reg_gener, regs)?;
+                ret.extend(insts);
+                offset = new_offset;
+                dims = new_dims;
+            } else {
+                break;
             }
-        }
-        for idx in gep.indices.iter().skip(1) {
-            todo!("gep with multiple indices");
         }
 
         let final_offset = reg_gener.gen_virtual_usual_reg();
@@ -97,6 +78,95 @@ impl IRBuilder {
         regs.insert(gep.dest.clone(), final_addr);
 
         Ok(ret)
+    }
+
+    fn process_first_idx(
+        offset: Reg,
+        idx: &llvm_ir::Operand,
+        dims: &Dimension,
+        reg_gener: &mut RegGenerator,
+        regs: &mut HashMap<Name, Reg>,
+    ) -> Result<(Reg, Vec<Inst>)> {
+        let mut ret_insts: Vec<Inst> = Vec::new();
+        let mut offset = offset;
+
+        let idx = Self::value_from(idx, regs)?;
+
+        let factor: Imm = dims.size().try_into()?;
+        let (factor, pre_insert) = Self::prepare_imm_rhs(&factor, reg_gener)?;
+        ret_insts.extend(pre_insert);
+
+        match idx {
+            Operand::Imm(idx_imm) => {
+                let to_add = reg_gener.gen_virtual_usual_reg();
+                let idx_reg = reg_gener.gen_virtual_usual_reg();
+                let new_offset = reg_gener.gen_virtual_usual_reg();
+
+                let li: Inst = LiInst::new(idx_reg.into(), idx_imm.into()).into();
+                ret_insts.push(li);
+
+                let mul: Inst = MulInst::new(to_add.into(), idx_reg.into(), factor).into();
+                ret_insts.push(mul);
+
+                let add: Inst =
+                    AddInst::new(new_offset.into(), offset.into(), to_add.into()).into();
+                ret_insts.push(add);
+
+                offset = new_offset;
+            }
+            Operand::Reg(idx) => {
+                let to_add = reg_gener.gen_virtual_usual_reg();
+                let new_offset = reg_gener.gen_virtual_usual_reg();
+
+                let mul: Inst = MulInst::new(to_add.into(), idx.into(), factor).into();
+                ret_insts.push(mul);
+
+                let add: Inst =
+                    AddInst::new(new_offset.into(), offset.into(), to_add.into()).into();
+                ret_insts.push(add);
+
+                offset = new_offset;
+            }
+            _ => unimplemented!(),
+        }
+
+        Ok((offset, ret_insts))
+    }
+
+    #[allow(unused)]
+    fn process_sub_idx<'a>(
+        offset: Reg,
+        idx: &llvm_ir::Operand,
+        dims: &'a Dimension,
+        reg_gener: &mut RegGenerator,
+        regs: &mut HashMap<Name, Reg>,
+    ) -> Result<(Reg, Option<&'a Dimension>, Vec<Inst>)> {
+        let mut ret_insts: Vec<Inst> = Vec::new();
+        let mut offset = offset;
+        let mut ret_dims = None;
+
+        let idx = Self::value_from(idx, regs)?;
+        match idx {
+            Operand::Imm(idx_imm) => {
+                // 根据idx_imm的值,计算offset的增量
+                let idx_u: usize = idx_imm.try_into()?;
+                let factor: usize = dims.iter_subs().take(idx_u).map(|d| d.size()).sum();
+                let factor: Imm = factor.try_into()?;
+                let new_offset = reg_gener.gen_virtual_usual_reg();
+                let add = AddInst::new(new_offset.into(), offset.into(), factor.into());
+                ret_insts.push(add.into());
+                offset = new_offset;
+                ret_dims = dims.iter_subs().nth(idx_u);
+            }
+            Operand::Reg(idx) => {
+                unimplemented!();
+            }
+            _ => {
+                return Err(anyhow!("").context(context!()));
+            }
+        }
+
+        Ok((offset, ret_dims, ret_insts))
     }
 
     fn dims_from_gep_inst(gep: &llvm_ir::instruction::GetElementPtr) -> Result<Dimension> {
