23084dfc0b47b62aedb0c5c9545df0f986b59e41
5eqn <subcat2077@gmail.com>
2024-08-04 17:36:14 +0800

    :bug: phi in mem2reg does not remember operand

--- src/middle/transform/deadcode_elimination.rs
+++ src/middle/transform/deadcode_elimination.rs
@@ -1,40 +1,68 @@
 use anyhow::Result;
 
 use crate::middle::ir::instruction::InstType;
-use crate::middle::ir::{BBPtr, FunPtr, InstPtr};
-use crate::middle::{ir, Program};
+use crate::middle::ir::{BBPtr, FunPtr, InstPtr, Operand};
+use crate::middle::Program;
 
 #[allow(unused)]
 pub fn optimize_program(program: &mut Program) -> Result<()> {
-    deadcode_elimination(&mut program.module);
+    DCE::new(program).deadcode_elimination();
     Ok(())
 }
 
-#[allow(unused)]
-pub fn deadcode_elimination(modu: &mut ir::Module) {
-    modu.functions
-        .iter()
-        .filter(|f| !f.is_lib())
-        .for_each(|x| deadcode_elimination_func(*x));
+pub struct DCE<'a> {
+    program: &'a mut Program,
 }
 
-pub fn deadcode_elimination_func(func: FunPtr) {
+impl<'a> DCE<'a> {
+    pub fn new(program: &'a mut Program) -> Self {
+        Self { program }
+    }
+
+    pub fn deadcode_elimination(&mut self) {
+        self.program
+            .module
+            .functions
+            .clone()
+            .iter()
+            .filter(|f| !f.is_lib())
+            .for_each(deadcode_elimination_func);
+
+        // Global variable does not require revisit, remove unused variables at the end
+        self.program
+            .module
+            .global_variables
+            .retain(|x| !x.get_user().is_empty());
+    }
+}
+
+fn deadcode_elimination_func(func: &FunPtr) {
+    // Use post order traversal to reduce revisits
     func.po_iter().for_each(deadcode_elimination_block);
 }
-pub fn deadcode_elimination_block(bb: BBPtr) {
-    bb.iter_rev().for_each(deadcode_elimination_inst);
+
+fn deadcode_elimination_block(bb: BBPtr) {
+    // Iterate forward so that next instruction is always valid
+    bb.iter().for_each(deadcode_elimination_inst);
 }
-pub fn deadcode_elimination_inst(mut inst: InstPtr) {
+
+fn deadcode_elimination_inst(mut inst: InstPtr) {
     if !inst.get_user().is_empty() || has_side_effect(inst) {
-        // Do not eliminate used instruction or instruction with side effect
         return;
     }
+    let operands: Vec<_> = inst.get_operand().into();
     inst.remove_self();
-}
-fn has_side_effect(inst: InstPtr) -> bool {
-    match inst.get_type() {
-        // TODO pure function analysis
-        InstType::Store | InstType::Call | InstType::Ret | InstType::Br => true,
-        _ => false,
+    for op in operands {
+        if let Operand::Instruction(inst) = op {
+            deadcode_elimination_inst(inst);
+        }
     }
+}
+
+fn has_side_effect(inst: InstPtr) -> bool {
+    // TODO pure function analysis
+    matches!(
+        inst.get_type(),
+        InstType::Store | InstType::Call | InstType::Ret | InstType::Br
+    )
 }
