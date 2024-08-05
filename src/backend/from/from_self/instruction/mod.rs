6f2ff719c1824bab95800a267799cdb4f2fc1c97
wangfiox <wangfiox@gmail.com>
2024-08-05 19:06:28 +0800

    :art: 我们有消除无效指令, 因此build后端ir的时候

--- src/backend/from/from_self/instruction/mod.rs
+++ src/backend/from/from_self/instruction/mod.rs
@@ -10,9 +10,7 @@ pub use builder::IRBuilder;
 
 pub use crate::utils::mem::ObjPtr;
 
-pub use crate::{
-    backend::*, ssa2tac_three_float, ssa2tac_three_usual_Itype, ssa2tac_three_usual_Rtype,
-};
+pub use crate::{backend::*, ssa2tac_three_float, ssa2tac_three_usual_Itype};
 pub use crate::{context, middle};
 
 pub use crate::middle::ir::instruction::binary_inst::BinaryInst;
