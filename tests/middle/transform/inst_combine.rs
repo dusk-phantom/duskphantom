bbcd6ac521892491455ec9b37fe511949f2f83ae
5eqn <subcat2077@gmail.com>
2024-08-06 18:29:28 +0800

    :white_check_mark: update test

--- tests/middle/transform/inst_combine.rs
+++ tests/middle/transform/inst_combine.rs
@@ -69,4 +69,171 @@ pub mod tests_constant_fold {
         }
         "###);
     }
+
+    #[test]
+    fn test_add_sub() {
+        let code = r#"
+        int f(int x) {
+            int x1 = x + 1 - 4 + 6 + 8;
+            return x1;
+        }
+        "#;
+
+        // Check before optimization
+        let parsed = parse(code).unwrap();
+        let mut program = gen(&parsed).unwrap();
+        mem2reg::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        constant_fold::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_before = program.module.gen_llvm_ir();
+
+        // Check after optimization
+        inst_combine::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_after = program.module.gen_llvm_ir();
+        assert_snapshot!(diff(&llvm_before, &llvm_after),@r###"
+        declare i32 @getint()
+        declare i32 @getch()
+        declare float @getfloat()
+        declare void @putint(i32 %p0)
+        declare void @putch(i32 %p0)
+        declare void @putfloat(float %p0)
+        declare i32 @getarray(i32* %p0)
+        declare i32 @getfarray(float* %p0)
+        declare void @putarray(i32 %p0, i32* %p1)
+        declare void @putfarray(i32 %p0, float* %p1)
+        declare void @_sysy_starttime(i32 %p0)
+        declare void @_sysy_stoptime(i32 %p0)
+        declare void @putf()
+        declare void @llvm.memset.p0.i32(i32* %p0, i8 %p1, i32 %p2, i1 %p3)
+        define i32 @f(i32 %x) {
+        entry:
+        [-] %Add_9 = add i32 %x, 1
+        [-] %Sub_10 = sub i32 %Add_9, 4
+        [-] %Add_11 = add i32 %Sub_10, 6
+        [-] %Add_12 = add i32 %Add_11, 8
+        [+] %Add_19 = add i32 %x, 11
+        br label %exit
+
+        exit:
+        [-] ret i32 %Add_12
+        [+] ret i32 %Add_19
+
+
+        }
+        "###);
+    }
+
+    #[test]
+    fn test_shift() {
+        let code = r#"
+        int f(int x) {
+            int x1 = x * 2;
+            int x2 = x1 * 2;
+            int x3 = x2 * 2;
+            return x3;
+        }
+        "#;
+
+        // Check before optimization
+        let parsed = parse(code).unwrap();
+        let mut program = gen(&parsed).unwrap();
+        mem2reg::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        constant_fold::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_before = program.module.gen_llvm_ir();
+
+        // Check after optimization
+        inst_combine::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_after = program.module.gen_llvm_ir();
+        assert_snapshot!(diff(&llvm_before, &llvm_after),@r###"
+        declare i32 @getint()
+        declare i32 @getch()
+        declare float @getfloat()
+        declare void @putint(i32 %p0)
+        declare void @putch(i32 %p0)
+        declare void @putfloat(float %p0)
+        declare i32 @getarray(i32* %p0)
+        declare i32 @getfarray(float* %p0)
+        declare void @putarray(i32 %p0, i32* %p1)
+        declare void @putfarray(i32 %p0, float* %p1)
+        declare void @_sysy_starttime(i32 %p0)
+        declare void @_sysy_stoptime(i32 %p0)
+        declare void @putf()
+        declare void @llvm.memset.p0.i32(i32* %p0, i8 %p1, i32 %p2, i1 %p3)
+        define i32 @f(i32 %x) {
+        entry:
+        [-] %Mul_9 = mul i32 %x, 2
+        [-] %Mul_13 = mul i32 %Mul_9, 2
+        [-] %Mul_17 = mul i32 %Mul_13, 2
+        [+] %Shl_26 = shl i32 %x, 3
+        br label %exit
+
+        exit:
+        [-] ret i32 %Mul_17
+        [+] ret i32 %Shl_26
+
+
+        }
+        "###);
+    }
+
+    #[test]
+    fn test_div() {
+        let code = r#"
+        int f(int x) {
+            int x1 = x / x;
+            int x2 = x1 * x;
+            int x3 = x2 / 8;
+            return x3;
+        }
+        "#;
+
+        // Check before optimization
+        let parsed = parse(code).unwrap();
+        let mut program = gen(&parsed).unwrap();
+        mem2reg::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        constant_fold::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_before = program.module.gen_llvm_ir();
+
+        // Check after optimization
+        inst_combine::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_after = program.module.gen_llvm_ir();
+        assert_snapshot!(diff(&llvm_before, &llvm_after),@r###"
+        declare i32 @getint()
+        declare i32 @getch()
+        declare float @getfloat()
+        declare void @putint(i32 %p0)
+        declare void @putch(i32 %p0)
+        declare void @putfloat(float %p0)
+        declare i32 @getarray(i32* %p0)
+        declare i32 @getfarray(float* %p0)
+        declare void @putarray(i32 %p0, i32* %p1)
+        declare void @putfarray(i32 %p0, float* %p1)
+        declare void @_sysy_starttime(i32 %p0)
+        declare void @_sysy_stoptime(i32 %p0)
+        declare void @putf()
+        declare void @llvm.memset.p0.i32(i32* %p0, i8 %p1, i32 %p2, i1 %p3)
+        define i32 @f(i32 %x) {
+        entry:
+        [-] %SDiv_10 = sdiv i32 %x, %x
+        [-] %Mul_15 = mul i32 %SDiv_10, %x
+        [-] %SDiv_19 = sdiv i32 %Mul_15, 8
+        [+] %AShr_24 = ashr i32 %x, 3
+        br label %exit
+
+        exit:
+        [-] ret i32 %SDiv_19
+        [+] ret i32 %AShr_24
+
+
+        }
+        "###);
+    }
 }
