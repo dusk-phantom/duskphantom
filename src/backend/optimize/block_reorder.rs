91be604bd4c27403baccc7c870aeb2d546ae4ae5
cncsmonster <cncsmonster@163.com>
2024-08-06 23:19:00 +0800

    :white_check_mark: add test for block merge

--- src/backend/optimize/block_reorder.rs
+++ src/backend/optimize/block_reorder.rs
@@ -40,3 +40,114 @@ pub fn handle_single_jmp(func: &mut Func) -> Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use insta::{assert_debug_snapshot, assert_snapshot};
+
+    use crate::utils::diff::diff;
+
+    use super::*;
+    fn new_bb(label: &str) -> Block {
+        Block::new(label.to_string())
+    }
+    fn j(bb: &mut Block, label: &str) {
+        bb.push_inst(JmpInst::new(label.into()).into());
+    }
+    fn b(bb: &mut Block, label1: &str, label2: &str) {
+        bb.push_inst(BeqInst::new(REG_A1, REG_ZERO, label1.into()).into());
+        bb.push_inst(JmpInst::new(label2.into()).into());
+    }
+    #[test]
+    fn t1() {
+        let mut bb0 = new_bb("b0");
+        bb0.push_inst(MvInst::new(REG_A0.into(), REG_A1.into()).into());
+        j(&mut bb0, "b1");
+        let mut bb1 = new_bb("b1");
+        bb1.push_inst(MvInst::new(REG_A1.into(), REG_A2.into()).into());
+        j(&mut bb1, "b2");
+        let mut bb2 = new_bb("b2");
+        bb2.push_inst(MvInst::new(REG_A2.into(), REG_A3.into()).into());
+        j(&mut bb2, "b3");
+        let mut bb3 = new_bb("b3");
+        bb3.push_inst(Inst::Ret);
+        let mut f = Func::new("".to_string(), vec![], bb0);
+        f.push_bb(bb1);
+        f.push_bb(bb2);
+        f.push_bb(bb3);
+
+        let f_before = f.gen_asm();
+
+        handle_single_jmp(&mut f).unwrap();
+
+        let f_after = f.gen_asm();
+
+        assert_snapshot!(diff(&f_before,&f_after),@r###"
+        .text
+        .align	3
+        .globl	
+        .type	, @function
+        :
+        b0:
+        mv a0,a1
+        [-] j b1
+        [-] b1:
+        mv a1,a2
+        [-] j b2
+        [-] b2:
+        mv a2,a3
+        [-] j b3
+        [-] b3:
+        ret
+        .size	, .-
+        "###);
+    }
+
+    #[test]
+    fn t2() {
+        let mut bb0 = new_bb("b0");
+        bb0.push_inst(MvInst::new(REG_A0.into(), REG_A1.into()).into());
+        j(&mut bb0, "b1");
+        let mut bb1 = new_bb("b1");
+        bb1.push_inst(MvInst::new(REG_A1.into(), REG_A2.into()).into());
+        b(&mut bb1, "b2", "b3");
+        let mut bb2 = new_bb("b2");
+        bb2.push_inst(MvInst::new(REG_A2.into(), REG_A3.into()).into());
+        j(&mut bb2, "b3");
+        let mut bb3 = new_bb("b3");
+        bb3.push_inst(Inst::Ret);
+        let mut f = Func::new("".to_string(), vec![], bb0);
+        f.push_bb(bb1);
+        f.push_bb(bb2);
+        f.push_bb(bb3);
+
+        let f_before = format!("{:?}", &f);
+        let f_asm_before = f.gen_asm();
+
+        handle_single_jmp(&mut f).unwrap();
+        let f_after = format!("{:?}", &f);
+        let f_asm_after = f.gen_asm();
+
+        assert_snapshot!(diff(&f_asm_before,&f_asm_after),@r###"
+        .text
+        .align	3
+        .globl	
+        .type	, @function
+        :
+        b0:
+        mv a0,a1
+        [-] j b1
+        [-] b1:
+        mv a1,a2
+        beq a1,zero,b2
+        [-] j b3
+        [+] ret
+        b2:
+        mv a2,a3
+        j b3
+        [-] b3:
+        [-] ret
+        .size	, .-
+        "###);
+    }
+}
