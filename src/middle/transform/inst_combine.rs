ce0dda10021453aff35fa5a9f2c90232b0e2a15c
5eqn <491100866@qq.com>
2024-08-07 09:25:11 +0800

    :sparkles: more inst combine

--- src/middle/transform/inst_combine.rs
+++ src/middle/transform/inst_combine.rs
@@ -1,8 +1,14 @@
 use anyhow::Result;
 
-use crate::middle::{
-    ir::{instruction::InstType, Constant, InstPtr, Operand},
-    Program,
+use crate::{
+    backend::from_self::downcast_ref,
+    middle::{
+        ir::{
+            instruction::{memory_op_inst::GetElementPtr, InstType},
+            Constant, InstPtr, Operand,
+        },
+        Program,
+    },
 };
 
 pub fn optimize_program(program: &mut Program) -> Result<()> {
@@ -50,13 +56,19 @@ impl<'a> InstCombine<'a> {
                     self.make_shift(inst);
                 }
             }
+            for bb in fun.rpo_iter() {
+                for inst in bb.iter() {
+                    self.useless_elim(inst);
+                }
+            }
         }
     }
 
     fn useless_elim(&mut self, mut inst: InstPtr) {
         let inst_type = inst.get_type();
 
-        // Useless instruction elimination: x + 0, x - 0, x * 1, x / 1, x >> 0, x << 0
+        // Useless instruction elimination:
+        // x + 0, x - 0, x * 1, x / 1, x >> 0, x << 0, 0 / x, x * 0, phi x, ..., x
         match inst_type {
             InstType::Add | InstType::Sub => {
                 let lhs = inst.get_operand()[0].clone();
@@ -81,9 +93,12 @@ impl<'a> InstCombine<'a> {
             InstType::Mul | InstType::SDiv | InstType::UDiv => {
                 let lhs = inst.get_operand()[0].clone();
                 let rhs = inst.get_operand()[1].clone();
-                if let Operand::Constant(constant) = rhs {
-                    if constant == Constant::Int(1) {
+                if let Operand::Constant(rhs) = rhs {
+                    if rhs == Constant::Int(1) {
                         inst.replace_self(&lhs);
+                        return;
+                    } else if rhs == Constant::Int(0) {
+                        inst.replace_self(&rhs.into());
                         return;
                     }
                 }
@@ -91,9 +106,15 @@ impl<'a> InstCombine<'a> {
             InstType::FMul | InstType::FDiv => {
                 let lhs = inst.get_operand()[0].clone();
                 let rhs = inst.get_operand()[1].clone();
-                if let Operand::Constant(constant) = rhs {
-                    if constant == Constant::Float(1.0) {
+                if let Operand::Constant(rhs) = rhs {
+                    if rhs == Constant::Float(1.0) {
                         inst.replace_self(&lhs);
+                        return;
+                    }
+                }
+                if let Operand::Constant(lhs) = lhs {
+                    if lhs == Constant::Float(0.0) {
+                        inst.replace_self(&lhs.into());
                         return;
                     }
                 }
@@ -101,11 +122,25 @@ impl<'a> InstCombine<'a> {
             InstType::AShr | InstType::Shl => {
                 let lhs = inst.get_operand()[0].clone();
                 let rhs = inst.get_operand()[1].clone();
-                if let Operand::Constant(constant) = rhs {
-                    if constant == Constant::Int(0) {
+                if let Operand::Constant(rhs) = rhs {
+                    if rhs == Constant::Int(0) {
                         inst.replace_self(&lhs);
                         return;
                     }
+                }
+                if let Operand::Constant(lhs) = lhs {
+                    if lhs == Constant::Int(0) {
+                        inst.replace_self(&lhs.into());
+                        return;
+                    }
+                }
+            }
+            InstType::Phi => {
+                let first = inst.get_operand()[0].clone();
+                let all_same = inst.get_operand().iter().all(|op| *op == first);
+                if all_same {
+                    inst.replace_self(&first);
+                    return;
                 }
             }
             _ => (),
@@ -136,6 +171,48 @@ impl<'a> InstCombine<'a> {
     fn combine_inst(&mut self, mut inst: InstPtr) {
         let inst_type = inst.get_type();
 
+        // Merge GEP instruction
+        if inst_type == InstType::GetElementPtr {
+            let ptr = inst.get_operand()[0].clone();
+            let n = inst.get_operand().len() - 1;
+            if let Operand::Instruction(ptr) = ptr {
+                if ptr.get_type() == InstType::GetElementPtr {
+                    // Outer GEP: getelementptr ty1, inner, i1, ..., in
+                    // Inner GEP: getelementptr ty2, alloc, j1, ..., jm
+                    // Merged GEP: getelementptr ty2, alloc, j1, ..., jm + i1, ..., in
+                    let m = ptr.get_operand().len() - 1;
+
+                    // Create instruction for jm + i1
+                    let add = self
+                        .program
+                        .mem_pool
+                        .get_add(ptr.get_operand()[m].clone(), inst.get_operand()[1].clone());
+                    inst.insert_before(add);
+
+                    // Create a list of all operands
+                    let operands = [
+                        ptr.get_operand()[1..m].to_vec(),
+                        vec![add.into()],
+                        inst.get_operand()[2..].to_vec(),
+                    ]
+                    .concat();
+
+                    // Create new GEP instruction
+                    let gep = downcast_ref::<GetElementPtr>(ptr.as_ref().as_ref());
+                    let new_inst = self.program.mem_pool.get_getelementptr(
+                        gep.element_type.clone(),
+                        ptr.get_operand()[0].clone(),
+                        operands,
+                    );
+
+                    // Replace outer GEP with new GEP
+                    inst.insert_after(new_inst);
+                    inst.replace_self(&new_inst.into());
+                    return;
+                }
+            }
+        }
+
         // For commutative instructions, move constant to RHS
         match inst_type {
             InstType::Add | InstType::Mul | InstType::FAdd | InstType::FMul => {
@@ -160,11 +237,11 @@ impl<'a> InstCombine<'a> {
                 let new_inst = self.program.mem_pool.get_mul(lhs, Constant::Int(2).into());
                 inst.insert_after(new_inst);
                 inst.replace_self(&new_inst.into());
-                return self.combine_inst(new_inst);
+                return;
             }
         }
 
-        // Inst combine: x + 1 - 6 -> x - 5, x * 2 * 3 -> x * 6
+        // Inst combine: x + 1 - 6 -> x - 5, x * 2 * 3 -> x * 6, x / 2 / 3 -> x / 6
         match inst_type {
             InstType::Add | InstType::Sub => {
                 let lhs = inst.get_operand()[0].clone();
@@ -185,7 +262,7 @@ impl<'a> InstCombine<'a> {
                                     self.program.mem_pool.get_add(lhs_lhs, new_rhs.into());
                                 inst.insert_after(new_inst);
                                 inst.replace_self(&new_inst.into());
-                                return self.combine_inst(new_inst);
+                                return;
                             }
                         }
                     }
@@ -209,7 +286,41 @@ impl<'a> InstCombine<'a> {
                                     self.program.mem_pool.get_mul(lhs_lhs, new_rhs.into());
                                 inst.insert_after(new_inst);
                                 inst.replace_self(&new_inst.into());
-                                return self.combine_inst(new_inst);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            InstType::SDiv => {
+                let lhs = inst.get_operand()[0].clone();
+                let rhs = inst.get_operand()[1].clone();
+
+                // Check if "rhs is constant" and "lhs is div"
+                if let Operand::Constant(Constant::Int(rhs)) = rhs {
+                    if let Operand::Instruction(lhs) = lhs {
+                        if lhs.get_type() == InstType::SDiv {
+                            let lhs_lhs = lhs.get_operand()[0].clone();
+                            let lhs_rhs = lhs.get_operand()[1].clone();
+
+                            // Combine inst if "lhs_rhs is constant"
+                            if let Operand::Constant(Constant::Int(lhs_rhs)) = lhs_rhs {
+                                let (new_rhs, overflow) = lhs_rhs.overflowing_mul(rhs);
+
+                                // If overflow, instruction result is zero
+                                if overflow {
+                                    inst.replace_self(&Constant::Int(0).into());
+                                    return;
+                                }
+
+                                // Otherwise, combine division factors
+                                let new_inst = self
+                                    .program
+                                    .mem_pool
+                                    .get_sdiv(lhs_lhs, Constant::Int(new_rhs).into());
+                                inst.insert_after(new_inst);
+                                inst.replace_self(&new_inst.into());
+                                return;
                             }
                         }
                     }
--- tests/middle/transform/inst_combine.rs
+++ tests/middle/transform/inst_combine.rs
@@ -1,5 +1,5 @@
 #[cfg(test)]
-pub mod tests_constant_fold {
+pub mod tests_inst_combine {
     use insta::assert_snapshot;
 
     use compiler::{
@@ -10,6 +10,133 @@ pub mod tests_constant_fold {
         },
         utils::diff::diff,
     };
+
+    #[test]
+    fn test_merge_phi() {
+        let code = r#"
+        int f(int x) {
+            int a = getint();
+            if (x < 5) {
+                a = 1;
+            } else {
+                a = 1;
+            }
+            return a;
+        }
+        "#;
+
+        // Check before optimization
+        let parsed = parse(code).unwrap();
+        let mut program = gen(&parsed).unwrap();
+        mem2reg::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        constant_fold::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_before = program.module.gen_llvm_ir();
+
+        // Check after optimization
+        inst_combine::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_after = program.module.gen_llvm_ir();
+        assert_snapshot!(diff(&llvm_before, &llvm_after), @r###"
+        declare i32 @getint()
+        declare i32 @getch()
+        declare float @getfloat()
+        declare void @putint(i32 %p0)
+        declare void @putch(i32 %p0)
+        declare void @putfloat(float %p0)
+        declare i32 @getarray(i32* %p0)
+        declare i32 @getfarray(float* %p0)
+        declare void @putarray(i32 %p0, i32* %p1)
+        declare void @putfarray(i32 %p0, float* %p1)
+        declare void @_sysy_starttime(i32 %p0)
+        declare void @_sysy_stoptime(i32 %p0)
+        declare void @putf()
+        declare void @llvm.memset.p0.i32(i32* %p0, i8 %p1, i32 %p2, i1 %p3)
+        define i32 @f(i32 %x) {
+        entry:
+        %call_8 = call i32 @getint()
+        br label %cond0
+
+        cond0:
+        %icmp_16 = icmp slt i32 %x, 5
+        br i1 %icmp_16, label %then1, label %alt2
+
+        then1:
+        br label %final3
+
+        alt2:
+        br label %final3
+
+        final3:
+        [-] %phi_25 = phi i32 [1, %then1], [1, %alt2]
+        br label %exit
+
+        exit:
+        [-] ret i32 %phi_25
+        [+] ret i32 1
+
+
+        }
+        "###);
+    }
+
+    #[test]
+    fn test_merge_gep() {
+        let code = r#"
+        int f(int x[][5][5][5]) {
+            int y = getint();
+            return x[1][2][y][3];
+        }
+        "#;
+
+        // Check before optimization
+        let parsed = parse(code).unwrap();
+        let mut program = gen(&parsed).unwrap();
+        mem2reg::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        constant_fold::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_before = program.module.gen_llvm_ir();
+
+        // Check after optimization
+        inst_combine::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_after = program.module.gen_llvm_ir();
+        assert_snapshot!(diff(&llvm_before, &llvm_after),@r###"
+        declare i32 @getint()
+        declare i32 @getch()
+        declare float @getfloat()
+        declare void @putint(i32 %p0)
+        declare void @putch(i32 %p0)
+        declare void @putfloat(float %p0)
+        declare i32 @getarray(i32* %p0)
+        declare i32 @getfarray(float* %p0)
+        declare void @putarray(i32 %p0, i32* %p1)
+        declare void @putfarray(i32 %p0, float* %p1)
+        declare void @_sysy_starttime(i32 %p0)
+        declare void @_sysy_stoptime(i32 %p0)
+        declare void @putf()
+        declare void @llvm.memset.p0.i32(i32* %p0, i8 %p1, i32 %p2, i1 %p3)
+        define i32 @f([5 x [5 x [5 x i32]]]* %x) {
+        entry:
+        %call_8 = call i32 @getint()
+        [-] %getelementptr_12 = getelementptr [5 x [5 x [5 x i32]]], ptr %x, i32 1
+        [-] %getelementptr_13 = getelementptr [5 x [5 x [5 x i32]]], ptr %getelementptr_12, i32 0, i32 2
+        [-] %getelementptr_14 = getelementptr [5 x [5 x i32]], ptr %getelementptr_13, i32 0, i32 %call_8
+        [-] %getelementptr_15 = getelementptr [5 x i32], ptr %getelementptr_14, i32 0, i32 3
+        [-] %load_16 = load i32, ptr %getelementptr_15
+        [+] %getelementptr_24 = getelementptr [5 x [5 x [5 x i32]]], ptr %x, i32 1, i32 2, i32 %call_8, i32 3
+        [+] %load_16 = load i32, ptr %getelementptr_24
+        br label %exit
+
+        exit:
+        ret i32 %load_16
+
+
+        }
+        "###);
+    }
 
     #[test]
     fn test_normal() {
@@ -187,8 +314,10 @@ pub mod tests_constant_fold {
         int f(int x) {
             int x1 = x / x;
             int x2 = x1 * x;
-            int x3 = x2 / 8;
-            return x3;
+            int x3 = x2 / 2;
+            int x4 = x3 / 2;
+            int x5 = x4 / 2;
+            return x5;
         }
         "#;
 
@@ -224,13 +353,74 @@ pub mod tests_constant_fold {
         entry:
         [-] %SDiv_10 = sdiv i32 %x, %x
         [-] %Mul_15 = mul i32 %SDiv_10, %x
-        [-] %SDiv_19 = sdiv i32 %Mul_15, 8
-        [+] %AShr_24 = ashr i32 %x, 3
+        [-] %SDiv_19 = sdiv i32 %Mul_15, 2
+        [-] %SDiv_23 = sdiv i32 %SDiv_19, 2
+        [-] %SDiv_27 = sdiv i32 %SDiv_23, 2
+        [+] %AShr_36 = ashr i32 %x, 3
         br label %exit
 
         exit:
-        [-] ret i32 %SDiv_19
-        [+] ret i32 %AShr_24
+        [-] ret i32 %SDiv_27
+        [+] ret i32 %AShr_36
+
+
+        }
+        "###);
+    }
+
+    #[test]
+    fn test_div_overflow() {
+        let code = r#"
+        int f(int x0) {
+            int x1 = x0 / 256;
+            int x2 = x1 / 256;
+            int x3 = x2 / 256;
+            int x4 = x3 / 256;
+            int x5 = x4 / 256;
+            return x5;
+        }
+        "#;
+
+        // Check before optimization
+        let parsed = parse(code).unwrap();
+        let mut program = gen(&parsed).unwrap();
+        mem2reg::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        constant_fold::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_before = program.module.gen_llvm_ir();
+
+        // Check after optimization
+        inst_combine::optimize_program(&mut program).unwrap();
+        deadcode_elimination::optimize_program(&mut program).unwrap();
+        let llvm_after = program.module.gen_llvm_ir();
+        assert_snapshot!(diff(&llvm_before, &llvm_after),@r###"
+        declare i32 @getint()
+        declare i32 @getch()
+        declare float @getfloat()
+        declare void @putint(i32 %p0)
+        declare void @putch(i32 %p0)
+        declare void @putfloat(float %p0)
+        declare i32 @getarray(i32* %p0)
+        declare i32 @getfarray(float* %p0)
+        declare void @putarray(i32 %p0, i32* %p1)
+        declare void @putfarray(i32 %p0, float* %p1)
+        declare void @_sysy_starttime(i32 %p0)
+        declare void @_sysy_stoptime(i32 %p0)
+        declare void @putf()
+        declare void @llvm.memset.p0.i32(i32* %p0, i8 %p1, i32 %p2, i1 %p3)
+        define i32 @f(i32 %x0) {
+        entry:
+        [-] %SDiv_9 = sdiv i32 %x0, 256
+        [-] %SDiv_13 = sdiv i32 %SDiv_9, 256
+        [-] %SDiv_17 = sdiv i32 %SDiv_13, 256
+        [-] %SDiv_21 = sdiv i32 %SDiv_17, 256
+        [-] %SDiv_25 = sdiv i32 %SDiv_21, 256
+        br label %exit
+
+        exit:
+        [-] ret i32 %SDiv_25
+        [+] ret i32 0
 
 
         }
