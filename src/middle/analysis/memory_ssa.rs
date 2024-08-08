bd2b14859447b7411803825115571d8a62358583
5eqn <subcat2077@gmail.com>
2024-08-08 15:12:32 +0800

    :sparkles: gen llvm ir for memory ssa

--- src/middle/analysis/memory_ssa.rs
+++ src/middle/analysis/memory_ssa.rs
@@ -4,28 +4,111 @@ use crate::{
     middle::{
         analysis::dominator_tree::DominatorTree,
         ir::{BBPtr, FunPtr, InstPtr},
+        Program,
     },
     utils::mem::{ObjPool, ObjPtr},
 };
 
 use super::{alias_analysis::EffectRange, effect_analysis::EffectAnalysis};
 
-pub struct MemorySSA {
+pub type NodePtr = ObjPtr<Node>;
+
+pub struct MemorySSA<'a> {
     builder: MemorySSABuilder,
     inst_to_node: HashMap<InstPtr, NodePtr>,
     block_to_node: HashMap<BBPtr, NodePtr>,
-    effect_analysis: EffectAnalysis,
+    node_to_user: HashMap<NodePtr, HashSet<NodePtr>>,
+    effect_analysis: &'a EffectAnalysis,
+    program: &'a Program,
 }
 
 #[allow(unused)]
-impl MemorySSA {
-    pub fn run(&mut self, func: FunPtr) {
-        let entry = func.entry.unwrap();
+impl<'a> MemorySSA<'a> {
+    pub fn new(program: &'a Program, effect_analysis: &'a EffectAnalysis) -> Self {
+        let mut memory_ssa = Self {
+            builder: MemorySSABuilder {
+                node_pool: ObjPool::new(),
+                counter: 0,
+            },
+            inst_to_node: HashMap::new(),
+            block_to_node: HashMap::new(),
+            node_to_user: HashMap::new(),
+            effect_analysis,
+            program,
+        };
+        for func in program.module.functions.iter() {
+            memory_ssa.run(*func);
+        }
+        memory_ssa
+    }
+
+    pub fn get_user(&self, node: NodePtr) -> HashSet<NodePtr> {
+        self.node_to_user.get(&node).cloned().unwrap_or_default()
+    }
+
+    pub fn gen_llvm_ir(&self) -> String {
+        let mut result = String::new();
+        for func in self.program.module.functions.iter() {
+            if func.is_lib() {
+                continue;
+            }
+            result += &format!("MemorySSA for function: {}\n", func.name);
+            for bb in func.dfs_iter() {
+                result += &format!("{}:\n", bb.name);
+                if let Some(node) = self.block_to_node.get(&bb) {
+                    result += &self.gen_llvm_ir_node(*node);
+                    result += "\n";
+                }
+                for inst in bb.iter() {
+                    if let Some(node) = self.inst_to_node.get(&inst) {
+                        result += &self.gen_llvm_ir_node(*node);
+                        result += "\n";
+                    }
+                    result += &inst.gen_llvm_ir();
+                    result += "\n";
+                }
+                result += "\n";
+            }
+        }
+        result
+    }
+
+    pub fn gen_llvm_ir_node(&self, node: NodePtr) -> String {
+        match node.as_ref() {
+            Node::Entry(_) => "; 0".to_string(),
+            Node::Normal(id, use_node, def_node, _) => {
+                let mut result: Vec<String> = Vec::new();
+                if let Some(use_node) = use_node {
+                    result.push(format!("; MemoryUse({})", use_node.get_id()));
+                }
+                if let Some(def_node) = def_node {
+                    result.push(format!("; {} = MemoryDef({})", id, def_node.get_id()));
+                }
+                result.join("\n")
+            }
+            Node::Phi(id, arg, _) => {
+                let mut result = format!("; {} = MemoryPhi(", id);
+                for (bb, node) in arg {
+                    result.push_str(&format!("[{}, {}]", node.get_id(), bb.name));
+                }
+                result.push(')');
+                result
+            }
+        }
+    }
+
+    fn run(&mut self, func: FunPtr) {
+        let Some(entry) = func.entry else {
+            return;
+        };
+
+        // Insert empty phi nodes
         let phi_insertions = self.insert_empty_phi(func);
 
         // Add entry node
         let mut range_to_node = RangeToNode::new();
         let entry_node = self.builder.get_entry();
+        self.block_to_node.insert(entry, entry_node);
         range_to_node.insert(EffectRange::All, entry_node);
 
         // Add other nodes
@@ -64,9 +147,12 @@ impl MemorySSA {
         // Build MemorySSA for each node
         for inst in current_bb.iter() {
             if let Some(effect) = self.effect_analysis.inst_effect.get(&inst) {
-                let def_node = range_to_node.get(&effect.def_range.clone().into());
-                let use_node = range_to_node.get(&effect.use_range.clone().into());
-                self.create_normal_node(use_node, def_node, inst);
+                let def_range = effect.def_range.clone().into();
+                let use_range = effect.use_range.clone().into();
+                let def_node = range_to_node.get(&def_range);
+                let use_node = range_to_node.get(&use_range);
+                let new_node = self.create_normal_node(use_node, def_node, inst);
+                range_to_node.insert(def_range, new_node);
             }
         }
 
@@ -88,9 +174,13 @@ impl MemorySSA {
         use_node: Option<NodePtr>,
         def_node: Option<NodePtr>,
         inst: InstPtr,
-    ) {
+    ) -> NodePtr {
         let node = self.builder.get_normal_node(use_node, def_node, inst);
         self.inst_to_node.insert(inst, node);
+        if let Some(use_node) = use_node {
+            self.node_to_user.entry(use_node).or_default().insert(node);
+        }
+        node
     }
 
     /// Insert empty "phi" for basic blocks starting from `entry`
@@ -137,32 +227,40 @@ impl MemorySSA {
     }
 }
 
-pub type NodePtr = ObjPtr<Node>;
-
-pub struct MemorySSABuilder {
+struct MemorySSABuilder {
     node_pool: ObjPool<Node>,
+    counter: usize,
 }
 
 impl MemorySSABuilder {
-    pub fn new_node(&mut self, node: Node) -> NodePtr {
+    fn new_node(&mut self, node: Node) -> NodePtr {
         self.node_pool.alloc(node)
     }
 
-    pub fn get_entry(&mut self) -> NodePtr {
-        self.new_node(Node::Entry)
+    fn next_counter(&mut self) -> usize {
+        let counter = self.counter;
+        self.counter += 1;
+        counter
     }
 
-    pub fn get_normal_node(
+    fn get_entry(&mut self) -> NodePtr {
+        let next_counter = self.next_counter();
+        self.new_node(Node::Entry(next_counter))
+    }
+
+    fn get_normal_node(
         &mut self,
         use_node: Option<NodePtr>,
         def_node: Option<NodePtr>,
         inst: InstPtr,
     ) -> NodePtr {
-        self.new_node(Node::Normal(use_node, def_node, inst))
+        let next_counter = self.next_counter();
+        self.new_node(Node::Normal(next_counter, use_node, def_node, inst))
     }
 
-    pub fn get_phi(&mut self, range: EffectRange) -> NodePtr {
-        self.new_node(Node::Phi(Vec::new(), range))
+    fn get_phi(&mut self, range: EffectRange) -> NodePtr {
+        let next_counter = self.next_counter();
+        self.new_node(Node::Phi(next_counter, Vec::new(), range))
     }
 }
 
@@ -170,30 +268,38 @@ type PhiArg = (BBPtr, NodePtr);
 
 #[allow(unused)]
 pub enum Node {
-    Entry,
-    Normal(Option<NodePtr>, Option<NodePtr>, InstPtr),
-    Phi(Vec<PhiArg>, EffectRange),
+    Entry(usize),
+    Normal(usize, Option<NodePtr>, Option<NodePtr>, InstPtr),
+    Phi(usize, Vec<PhiArg>, EffectRange),
 }
 
 impl Node {
-    pub fn add_phi_arg(&mut self, arg: PhiArg) {
+    fn add_phi_arg(&mut self, arg: PhiArg) {
         match self {
-            Node::Phi(args, _) => args.push(arg),
+            Node::Phi(_, args, _) => args.push(arg),
             _ => panic!("not a phi node"),
         }
     }
 
-    pub fn get_effect_range(&self) -> &EffectRange {
+    fn get_effect_range(&self) -> &EffectRange {
         match self {
-            Node::Phi(_, range) => range,
+            Node::Phi(_, _, range) => range,
             _ => panic!("not a phi node"),
         }
     }
 
-    pub fn merge_effect_range(&mut self, another: &EffectRange) {
+    fn merge_effect_range(&mut self, another: &EffectRange) {
         match self {
-            Node::Phi(_, range) => range.merge(another),
+            Node::Phi(_, _, range) => range.merge(another),
             _ => panic!("not a phi node"),
+        }
+    }
+
+    fn get_id(&self) -> usize {
+        match self {
+            Node::Entry(id) => *id,
+            Node::Normal(id, _, _, _) => *id,
+            Node::Phi(id, _, _) => *id,
         }
     }
 }
@@ -276,6 +382,9 @@ impl<'a> RangeToNode<'a> {
 
     /// Insert a new element into the last frame.
     pub fn insert(&mut self, k: EffectRange, v: NodePtr) {
+        if k.is_empty() {
+            return;
+        }
         self.last_frame().insert(k, v);
     }
 
