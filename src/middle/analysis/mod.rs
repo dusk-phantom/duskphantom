fe51317dd70edf00068ac25953819b3f73cee649
5eqn <491100866@qq.com>
2024-08-08 09:33:16 +0800

    :construction: alias analysis

--- src/middle/analysis/mod.rs
+++ src/middle/analysis/mod.rs
@@ -2,7 +2,9 @@ use crate::middle::ir::BBPtr;
 use crate::middle::ir::FunPtr;
 use std::collections::{HashMap, HashSet};
 
+pub mod alias;
 pub mod call_graph;
 pub mod dominator_tree;
+pub mod effect;
 pub mod loop_tools;
 pub mod reachability;
