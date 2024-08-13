4d37a81f57b151c0fb213751f6dd6d07407177cd
5eqn <subcat2077@gmail.com>
2024-08-13 15:49:11 +0800

    :white_check_mark: split gep test

--- src/middle/transform/load_store_elim.rs
+++ src/middle/transform/load_store_elim.rs
@@ -10,6 +10,7 @@ use super::{load_elim, store_elim};
 pub fn optimize_program(program: &mut Program) -> Result<bool> {
     let effect_analysis = EffectAnalysis::new(program);
     let mut memory_ssa = MemorySSA::new(program, &effect_analysis);
+    println!("{}", memory_ssa.dump());
     let mut changed = false;
     changed |= load_elim::optimize_program(program, &mut memory_ssa)?;
     changed |= store_elim::optimize_program(program, &mut memory_ssa)?;
