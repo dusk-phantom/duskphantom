b2388d74e0026ceb7f3313a7b19e895886f4f799
5eqn <subcat2077@gmail.com>
2024-08-15 09:11:42 +0800

    :white_check_mark: update test

--- tests/middle/analysis/memory_ssa.rs
+++ tests/middle/analysis/memory_ssa.rs
@@ -28,7 +28,7 @@ pub mod tests_memory_ssa {
         let memory_ssa = MemorySSA::new(&program, &effect_analysis);
         assert_snapshot!(memory_ssa.dump(), @r###"
         MemorySSA for function: main
-        entry:
+        entry:    ; preds = 
         ; 0 (liveOnEntry)
         ; 1 = MemoryDef(0)
         store i32 6, ptr @a
@@ -36,7 +36,7 @@ pub mod tests_memory_ssa {
         %load_6 = load i32, ptr @a
         br label %exit
 
-        exit:
+        exit:    ; preds = entry
         ret i32 %load_6
 
         "###);
@@ -61,29 +61,29 @@ pub mod tests_memory_ssa {
         let memory_ssa = MemorySSA::new(&program, &effect_analysis);
         assert_snapshot!(memory_ssa.dump(), @r###"
         MemorySSA for function: main
-        entry:
+        entry:    ; preds = 
         ; 0 (liveOnEntry)
         br label %cond0
 
-        cond0:
+        cond0:    ; preds = entry
         ; MemoryUse(0)
         %load_10 = load i32, ptr @a
         %icmp_11 = icmp slt i32 %load_10, 6
         br i1 %icmp_11, label %then1, label %alt2
 
-        alt2:
+        alt2:    ; preds = cond0
         br label %final3
 
-        final3:
+        final3:    ; preds = then1, alt2
         ; 1 = MemoryPhi([3, then1], [0, alt2])
         ; MemoryUse(1)
         %load_16 = load i32, ptr @a
         br label %exit
 
-        exit:
+        exit:    ; preds = final3
         ret i32 %load_16
 
-        then1:
+        then1:    ; preds = cond0
         ; 3 = MemoryDef(0)
         store i32 8, ptr @a
         br label %final3
@@ -112,7 +112,7 @@ pub mod tests_memory_ssa {
         let memory_ssa = MemorySSA::new(&program, &effect_analysis);
         assert_snapshot!(memory_ssa.dump(), @r###"
         MemorySSA for function: main
-        entry:
+        entry:    ; preds = 
         ; 0 (liveOnEntry)
         %getelementptr_5 = getelementptr [3 x [3 x i32]], ptr @a, i32 0, i32 2
         %getelementptr_6 = getelementptr [3 x i32], ptr %getelementptr_5, i32 0, i32 2
@@ -128,7 +128,7 @@ pub mod tests_memory_ssa {
         %load_14 = load i32, ptr %getelementptr_6
         br label %exit
 
-        exit:
+        exit:    ; preds = entry
         ret i32 %load_14
 
         "###);
@@ -155,7 +155,7 @@ pub mod tests_memory_ssa {
         let memory_ssa = MemorySSA::new(&program, &effect_analysis);
         assert_snapshot!(memory_ssa.dump(), @r###"
         MemorySSA for function: main
-        entry:
+        entry:    ; preds = 
         ; 0 (liveOnEntry)
         %getelementptr_5 = getelementptr [3 x [3 x i32]], ptr @a, i32 0, i32 2
         %getelementptr_6 = getelementptr [3 x i32], ptr %getelementptr_5, i32 0, i32 2
@@ -171,7 +171,7 @@ pub mod tests_memory_ssa {
         %load_14 = load i32, ptr %getelementptr_6
         br label %exit
 
-        exit:
+        exit:    ; preds = entry
         ret i32 %load_14
 
         "###);
@@ -201,17 +201,17 @@ pub mod tests_memory_ssa {
         let memory_ssa = MemorySSA::new(&program, &effect_analysis);
         assert_snapshot!(memory_ssa.dump(), @r###"
         MemorySSA for function: f
-        entry:
+        entry:    ; preds = 
         ; 0 (liveOnEntry)
         ; 1 = MemoryDef(0)
         store i32 3, ptr @b
         br label %exit
 
-        exit:
+        exit:    ; preds = entry
         ret void
 
         MemorySSA for function: main
-        entry:
+        entry:    ; preds = 
         ; 2 (liveOnEntry)
         ; 3 = MemoryDef(2)
         store i32 2, ptr @a
@@ -221,7 +221,7 @@ pub mod tests_memory_ssa {
         %load_12 = load i32, ptr @a
         br label %exit
 
-        exit:
+        exit:    ; preds = entry
         ret i32 %load_12
 
         "###);
@@ -252,7 +252,7 @@ pub mod tests_memory_ssa {
         let memory_ssa = MemorySSA::new(&program, &effect_analysis);
         assert_snapshot!(memory_ssa.dump(), @r###"
         MemorySSA for function: f
-        entry:
+        entry:    ; preds = 
         ; 0 (liveOnEntry)
         ; 1 = MemoryDef(0)
         store i32 9, ptr @a
@@ -260,11 +260,11 @@ pub mod tests_memory_ssa {
         store i32 3, ptr @b
         br label %exit
 
-        exit:
+        exit:    ; preds = entry
         ret void
 
         MemorySSA for function: main
-        entry:
+        entry:    ; preds = 
         ; 3 (liveOnEntry)
         ; 4 = MemoryDef(3)
         store i32 2, ptr @a
@@ -274,7 +274,7 @@ pub mod tests_memory_ssa {
         %load_13 = load i32, ptr @a
         br label %exit
 
-        exit:
+        exit:    ; preds = entry
         ret i32 %load_13
 
         "###);
