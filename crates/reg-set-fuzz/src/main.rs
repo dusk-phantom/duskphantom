403b86f3864620b362233b5fc965fa73027206c5
cncsmonster <cncsmonster@163.com>
2024-08-15 18:37:27 +0800

    :bug: 关掉使用有问题的RegSet的寄存器分配方法; :construct: 完善reg intervals

--- crates/reg-set-fuzz/src/main.rs
+++ crates/reg-set-fuzz/src/main.rs
@@ -18,7 +18,7 @@ fn tt() {
     apply_single_action(
         &mut rg,
         &mut rge,
-        reg_set_fuzz::SingleAction::Merge(vec![reg_set_fuzz::MReg(Reg::new(0, false))]),
+        reg_set_fuzz::SingleAction::Merge(vec![Reg::new(0, false).into()]),
     );
     single_check(&rg, &rge);
 }
@@ -28,12 +28,35 @@ fn tt2() {
     let mut rg = RegSet::new();
     let mut rge: HashSet<Reg> = HashSet::new();
     let actions = [
-        SingleAction::Insert(vec![MReg(Reg::new(372, false))]),
-        SingleAction::RetainInVec(vec![MReg(Reg::new(0, false))]),
+        SingleAction::Insert(vec![Reg::new(372, false).into()]),
+        SingleAction::RetainInVec(vec![Reg::new(0, false).into()]),
     ];
     apply_single_action(&mut rg, &mut rge, actions[0].clone());
     apply_single_action(&mut rg, &mut rge, actions[1].clone());
     // dbg!(&rg);
     // dbg!(&rge);
     single_check(&rg, &rge);
+}
+
+#[test]
+fn tt3() {
+    use SingleAction::*;
+    let action = Merge(vec![
+        MReg {
+            id: 7,
+            is_usual: false,
+        },
+        MReg {
+            id: 0,
+            is_usual: false,
+        },
+    ]);
+    let mut rs = RegSet::new();
+    let mut rse = HashSet::new();
+    println!("{}", std::mem::size_of_val(&rs));
+    print!("{}", std::mem::size_of_val(&rse));
+    dbg!(&rs.float, &rs.usual);
+    dbg!(&rs, &rse);
+    apply_single_action(&mut rs, &mut rse, action);
+    single_check(&rs, &rse);
 }
