22f3ca9521c19693395e24ee2faae7c250161338
5eqn <subcat2077@gmail.com>
2024-08-16 17:08:28 +0800

    :sparkles: redundant load elim

--- tests/middle/transform/redundance_elim.rs
+++ tests/middle/transform/redundance_elim.rs
@@ -15,6 +15,71 @@ pub mod tests_constant_fold {
     };
 
     #[test]
+    fn test_redundant_load() {
+        let code = r#"
+        int main() {
+            int x[8];
+            getarray(x);
+            return x[4] + x[4] + x[4] + x[4];
+        }
+        "#;
+
+        // Check before optimization
+        let parsed = parse(code).unwrap();
+        let mut program = gen(&parsed).unwrap();
+        mem2reg::optimize_program(&mut program).unwrap();
+        dead_code_elim::optimize_program(&mut program).unwrap();
+        let llvm_before = program.module.gen_llvm_ir();
+
+        // Check after optimization
+        redundance_elim::optimize_program(&mut program).unwrap();
+        inst_combine::optimize_program(&mut program).unwrap();
+        dead_code_elim::optimize_program(&mut program).unwrap();
+        let llvm_after = program.module.gen_llvm_ir();
+        assert_snapshot!(diff(&llvm_before, &llvm_after),@r###"
+        declare i32 @getint()
+        declare i32 @getch()
+        declare float @getfloat()
+        declare void @putint(i32 %p0)
+        declare void @putch(i32 %p0)
+        declare void @putfloat(float %p0)
+        declare i32 @getarray(i32* %p0)
+        declare i32 @getfarray(float* %p0)
+        declare void @putarray(i32 %p0, i32* %p1)
+        declare void @putfarray(i32 %p0, float* %p1)
+        declare void @_sysy_starttime(i32 %p0)
+        declare void @_sysy_stoptime(i32 %p0)
+        declare void @putf()
+        declare void @llvm.memset.p0.i32(i32* %p0, i8 %p1, i32 %p2, i1 %p3)
+        define i32 @main() {
+        entry:
+        %alloca_5 = alloca [8 x i32]
+        %getelementptr_6 = getelementptr [8 x i32], ptr %alloca_5, i32 0, i32 0
+        %call_7 = call i32 @getarray(i32* %getelementptr_6)
+        %getelementptr_8 = getelementptr [8 x i32], ptr %alloca_5, i32 0, i32 4
+        [-] %getelementptr_9 = getelementptr [8 x i32], ptr %alloca_5, i32 0, i32 4
+        %load_10 = load i32, ptr %getelementptr_8
+        [-] %load_11 = load i32, ptr %getelementptr_9
+        [-] %Add_12 = add i32 %load_10, %load_11
+        [-] %getelementptr_13 = getelementptr [8 x i32], ptr %alloca_5, i32 0, i32 4
+        [-] %load_14 = load i32, ptr %getelementptr_13
+        [-] %Add_15 = add i32 %Add_12, %load_14
+        [-] %getelementptr_16 = getelementptr [8 x i32], ptr %alloca_5, i32 0, i32 4
+        [-] %load_17 = load i32, ptr %getelementptr_16
+        [-] %Add_18 = add i32 %Add_15, %load_17
+        [+] %Mul_23 = mul i32 %load_10, 4
+        br label %exit
+
+        exit:
+        [-] ret i32 %Add_18
+        [+] ret i32 %Mul_23
+
+
+        }
+        "###);
+    }
+
+    #[test]
     fn test_normal() {
         let code = r#"
         int main() {
