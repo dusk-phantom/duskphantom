798da7e3aa377258c634723c39942665978b025e
5eqn <subcat2077@gmail.com>
2024-08-17 10:43:30 +0800

    :sparkles: overlapped store elim

--- tests/middle/transform/store_elim.rs
+++ tests/middle/transform/store_elim.rs
@@ -8,12 +8,86 @@ pub mod tests_store_elim {
             analysis::{effect_analysis::EffectAnalysis, memory_ssa::MemorySSA},
             irgen::gen,
             transform::{
-                block_fuse, constant_fold, dead_code_elim, func_inline, load_elim, load_store_elim,
-                mem2reg, redundance_elim, store_elim, inst_combine,
+                block_fuse, constant_fold, dead_code_elim, func_inline, inst_combine, load_elim,
+                load_store_elim, mem2reg, redundance_elim, store_elim,
             },
         },
         utils::diff::diff,
     };
+
+    #[test]
+    fn test_gvar_redundant_store() {
+        let code = r#"
+        int a;
+        int f() {
+            return 0;
+        }
+        int main() {
+            a = 2;
+            a = 3;
+            a = 5;
+            a = 8;
+            f();
+            putint(a);
+            return 0;
+        }
+        "#;
+
+        // Check before optimization
+        let parsed = parse(code).unwrap();
+        let mut program = gen(&parsed).unwrap();
+        mem2reg::optimize_program(&mut program).unwrap();
+        redundance_elim::optimize_program(&mut program).unwrap();
+        dead_code_elim::optimize_program(&mut program).unwrap();
+        let effect_analysis = EffectAnalysis::new(&program);
+        let mut memory_ssa = MemorySSA::new(&program, &effect_analysis);
+        let llvm_before = program.module.gen_llvm_ir();
+
+        // Check after optimization
+        store_elim::optimize_program(&mut program, &mut memory_ssa).unwrap();
+        let llvm_after = program.module.gen_llvm_ir();
+        assert_snapshot!(diff(&llvm_before, &llvm_after),@r###"
+        @a = dso_local global i32 0
+        declare i32 @getint()
+        declare i32 @getch()
+        declare float @getfloat()
+        declare void @putint(i32 %p0)
+        declare void @putch(i32 %p0)
+        declare void @putfloat(float %p0)
+        declare i32 @getarray(i32* %p0)
+        declare i32 @getfarray(float* %p0)
+        declare void @putarray(i32 %p0, i32* %p1)
+        declare void @putfarray(i32 %p0, float* %p1)
+        declare void @_sysy_starttime(i32 %p0)
+        declare void @_sysy_stoptime(i32 %p0)
+        declare void @putf()
+        declare void @llvm.memset.p0.i32(i32* %p0, i8 %p1, i32 %p2, i1 %p3)
+        define i32 @f() {
+        entry:
+        br label %exit
+
+        exit:
+        ret i32 0
+
+
+        }
+        define i32 @main() {
+        entry:
+        [-] store i32 2, ptr @a
+        [-] store i32 3, ptr @a
+        [-] store i32 5, ptr @a
+        store i32 8, ptr @a
+        %load_17 = load i32, ptr @a
+        call void @putint(i32 %load_17)
+        br label %exit
+
+        exit:
+        ret i32 0
+
+
+        }
+        "###);
+    }
 
     #[test]
     fn test_gvar_store() {
@@ -299,7 +373,6 @@ pub mod tests_store_elim {
         load_store_elim::optimize_program(&mut program).unwrap();
         redundance_elim::optimize_program(&mut program).unwrap();
         load_store_elim::optimize_program(&mut program).unwrap();
-        block_fuse::optimize_program(&mut program).unwrap();
         let llvm_after = program.module.gen_llvm_ir();
         assert_snapshot!(diff(&llvm_before, &llvm_after),@r###"
         @a = dso_local global [3 x [3 x i32]] zeroinitializer
@@ -335,6 +408,74 @@ pub mod tests_store_elim {
         [-] %load_20 = load i32, ptr %getelementptr_19
         [-] ret i32 %load_20
         [+] ret i32 2
+
+
+        }
+        "###);
+    }
+
+    #[test]
+    fn test_array_redundant_store() {
+        let code = r#"
+        int a[3][3];
+        int main() {
+            int x = getint();
+            int y = getint();
+            a[1][x] = 3;
+            a[1][y] = 5;
+            a[1][x] = 7;
+            a[1][y] = a[1][x];
+            putarray(9, a);
+            return 0;
+        }
+        "#;
+
+        // Check before optimization
+        let parsed = parse(code).unwrap();
+        let mut program = gen(&parsed).unwrap();
+        mem2reg::optimize_program(&mut program).unwrap();
+        redundance_elim::optimize_program(&mut program).unwrap();
+        dead_code_elim::optimize_program(&mut program).unwrap();
+        let llvm_before = program.module.gen_llvm_ir();
+
+        // Check after optimization
+        load_store_elim::optimize_program(&mut program).unwrap();
+        let llvm_after = program.module.gen_llvm_ir();
+        assert_snapshot!(diff(&llvm_before, &llvm_after),@r###"
+        @a = dso_local global [3 x [3 x i32]] zeroinitializer
+        declare i32 @getint()
+        declare i32 @getch()
+        declare float @getfloat()
+        declare void @putint(i32 %p0)
+        declare void @putch(i32 %p0)
+        declare void @putfloat(float %p0)
+        declare i32 @getarray(i32* %p0)
+        declare i32 @getfarray(float* %p0)
+        declare void @putarray(i32 %p0, i32* %p1)
+        declare void @putfarray(i32 %p0, float* %p1)
+        declare void @_sysy_starttime(i32 %p0)
+        declare void @_sysy_stoptime(i32 %p0)
+        declare void @putf()
+        declare void @llvm.memset.p0.i32(i32* %p0, i8 %p1, i32 %p2, i1 %p3)
+        define i32 @main() {
+        entry:
+        %call_6 = call i32 @getint()
+        %call_9 = call i32 @getint()
+        %getelementptr_12 = getelementptr [3 x [3 x i32]], ptr @a, i32 0, i32 1
+        %getelementptr_13 = getelementptr [3 x i32], ptr %getelementptr_12, i32 0, i32 %call_6
+        [-] store i32 3, ptr %getelementptr_13
+        %getelementptr_17 = getelementptr [3 x i32], ptr %getelementptr_12, i32 0, i32 %call_9
+        [-] store i32 5, ptr %getelementptr_17
+        store i32 7, ptr %getelementptr_13
+        [-] %load_29 = load i32, ptr %getelementptr_13
+        [-] store i32 %load_29, ptr %getelementptr_17
+        [+] store i32 7, ptr %getelementptr_17
+        %getelementptr_31 = getelementptr [3 x [3 x i32]], ptr @a, i32 0, i32 0
+        call void @putarray(i32 9, [3 x i32]* %getelementptr_31)
+        br label %exit
+
+        exit:
+        ret i32 0
 
 
         }
