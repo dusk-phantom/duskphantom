679ba38b63f71992a63239f7169f6405bb591079
cncsmonster <cncsmonster@163.com>
2024-08-20 02:24:00 +0800

    :sparkles: 提供环境变量配置 是否开启 自动并行化中端优化, 默认是开启, 以方便测试[f,hf]; 当测试f,hf的时候,设置OPEN_AUTO_PARALLEL=false 即可

--- src/config.rs
+++ src/config.rs
@@ -7,6 +7,7 @@ pub struct Config {
     pub num_parallel_for_func_gen_asm: usize,
     pub num_parallel_for_block_gen_asm: usize,
     pub reg_alloc_algo: String,
+    pub open_auto_parallel: bool,
 }
 
 lazy_static! {
@@ -15,11 +16,26 @@ lazy_static! {
             let reader = std::io::BufReader::new(file);
             serde_yaml::from_reader(reader).unwrap()
         } else {
+            use std::env;
             Config {
-                num_parallel_for_global_gen_asm: 12,
-                num_parallel_for_func_gen_asm: 4,
-                num_parallel_for_block_gen_asm: 3,
-                reg_alloc_algo: "graph-coloring".to_string(),
+                num_parallel_for_global_gen_asm: env::var("NUM_PARALLEL_FOR_GLOBAL_GEN_ASM")
+                    .unwrap_or_else(|_| "12".to_string())
+                    .parse()
+                    .unwrap_or(12),
+                num_parallel_for_func_gen_asm: env::var("NUM_PARALLEL_FOR_FUNC_GEN_ASM")
+                    .unwrap_or_else(|_| "4".to_string())
+                    .parse()
+                    .unwrap_or(4),
+                num_parallel_for_block_gen_asm: env::var("NUM_PARALLEL_FOR_BLOCK_GEN_ASM")
+                    .unwrap_or_else(|_| "3".to_string())
+                    .parse()
+                    .unwrap_or(3),
+                reg_alloc_algo: env::var("REG_ALLOC_ALGO")
+                    .unwrap_or_else(|_| "graph-coloring".to_string()),
+                open_auto_parallel: env::var("OPEN_AUTO_PARALLEL")
+                    .unwrap_or_else(|_| "true".to_string())
+                    .parse()
+                    .unwrap_or(false),
             }
         }
     };
