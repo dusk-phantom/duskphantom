21e11a31cf44257863adfbfc4f64e0f25e506be0
5eqn <subcat2077@gmail.com>
2024-08-19 21:45:53 +0800

    :white_check_mark: add test

--- tests/middle/transform/loop_optimization.rs
+++ tests/middle/transform/loop_optimization.rs
@@ -45,6 +45,58 @@ pub mod tests_make_parallel {
         dead_code_elim::optimize_program(&mut program).unwrap();
         let llvm_after = program.module.gen_llvm_ir();
         assert_snapshot!(diff(&llvm_before, &llvm_after), @r###"
+        @A = dso_local global [9 x i32] zeroinitializer
+        @B = dso_local global [9 x i32] zeroinitializer
+        declare i32 @getint()
+        declare i32 @getch()
+        declare float @getfloat()
+        declare void @putint(i32 %p0)
+        declare void @putch(i32 %p0)
+        declare void @putfloat(float %p0)
+        declare i32 @getarray(i32* %p0)
+        declare i32 @getfarray(float* %p0)
+        declare void @putarray(i32 %p0, i32* %p1)
+        declare void @putfarray(i32 %p0, float* %p1)
+        declare void @_sysy_starttime(i32 %p0)
+        declare void @_sysy_stoptime(i32 %p0)
+        declare i32 @thrd_create(i32 %p0)
+        declare void @thrd_join()
+        declare void @putf()
+        declare void @llvm.memset.p0.i32(i32* %p0, i8 %p1, i32 %p2, i1 %p3)
+        define i32 @main() {
+        entry:
+        %getelementptr_6 = getelementptr [9 x i32], ptr @A, i32 0, i32 0
+        %call_7 = call i32 @getarray(i32* %getelementptr_6)
+        [+] %Sub_27 = sub i32 %call_7, 1
+        [+] %getelementptr_28 = getelementptr [9 x i32], ptr @A, i32 0, i32 %Sub_27
+        [+] %load_30 = load i32, ptr %getelementptr_28
+        br label %cond0
+
+        cond0:
+        %phi_38 = phi i32 [0, %entry], [%Add_23, %body1]
+        [-] %Sub_27 = sub i32 %call_7, 1
+        [-] %getelementptr_28 = getelementptr [9 x i32], ptr @A, i32 0, i32 %Sub_27
+        [-] %load_30 = load i32, ptr %getelementptr_28
+        %icmp_31 = icmp slt i32 %phi_38, %load_30
+        br i1 %icmp_31, label %body1, label %final2
+
+        body1:
+        %getelementptr_16 = getelementptr [9 x i32], ptr @B, i32 0, i32 %phi_38
+        %load_17 = load i32, ptr %getelementptr_16
+        %Add_18 = add i32 %load_17, 1
+        store i32 %Add_18, ptr %getelementptr_16
+        %Add_23 = add i32 %phi_38, 1
+        br label %cond0
+
+        final2:
+        call void @putarray(i32 %call_7, i32* %getelementptr_6)
+        br label %exit
+
+        exit:
+        ret i32 0
+
+
+        }
         "###);
     }
 }
